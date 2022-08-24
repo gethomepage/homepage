d54561cec93ce58310c31b3b7f602fa21d962e9f
Ben Phelps <ben@phelps.io>
2022-08-25 02:42:33 +0300

    cleanup formatting

--- src/utils/stats-helpers.js
+++ src/utils/stats-helpers.js
@@ -1,10 +1,7 @@
 export function calculateCPUPercent(stats) {
   let cpuPercent = 0.0;
-  const cpuDelta =
-    stats.cpu_stats.cpu_usage.total_usage -
-    stats.precpu_stats.cpu_usage.total_usage;
-  const systemDelta =
-    stats.cpu_stats.system_cpu_usage - stats.precpu_stats.system_cpu_usage;
+  const cpuDelta = stats.cpu_stats.cpu_usage.total_usage - stats.precpu_stats.cpu_usage.total_usage;
+  const systemDelta = stats.cpu_stats.system_cpu_usage - stats.precpu_stats.system_cpu_usage;
 
   if (systemDelta > 0.0 && cpuDelta > 0.0) {
     cpuPercent = (cpuDelta / systemDelta) * stats.cpu_stats.online_cpus * 100.0;
