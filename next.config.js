374b99322eb29f7a7fa27fcaacd928c8654818c4
Ben Phelps <ben@phelps.io>
2022-08-25 18:37:13 +0300

    disable SWC minify
    too experimental right now for arm/v6/v7 support

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
-  swcMinify: true,
+  swcMinify: false,
   experimental: { images: { allowFutureImage: true, unoptimized: true } },
   images: {
     domains: ["cdn.jsdelivr.net"],
