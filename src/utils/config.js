affd1e97c2b0c478f87526f31e5d59e1914d07ed
Ben Phelps <ben@phelps.io>
2022-08-25 14:07:14 +0300

    remove debug lines

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,11 +3,8 @@ import { existsSync, copyFile } from "fs";
 
 export default function checkAndCopyConfig(config) {
   const configYaml = join(process.cwd(), "config", config);
-  console.log("config:", configYaml);
   if (!existsSync(configYaml)) {
-    console.log("does not exist");
     const configSkeleton = join(process.cwd(), "src", "skeleton", config);
-    console.log("skeleton", configSkeleton);
     copyFile(configSkeleton, configYaml, (err) => {
       if (err) {
         console.log("error copying config", err);
