bfc307efbee6596f1adea1fb29eff75ecd530711
Ben Phelps <ben@phelps.io>
2022-08-25 13:16:03 +0300

    debug fs steps

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,10 +3,16 @@ import { existsSync, copyFile } from "fs";
 
 export default function checkAndCopyConfig(config) {
   const configYaml = join(process.cwd(), "config", config);
+  console.log("config:", configYaml);
   if (!existsSync(configYaml)) {
+    console.log("does not exist");
     const configSkeleton = join(process.cwd(), "src", "skeleton", config);
+    console.log("skeleton", configSkeleton);
     copyFile(configSkeleton, configYaml, (err) => {
-      if (err) throw err;
+      if (err) {
+        console.log("error copying config", err);
+        throw err;
+      }
       console.info("%s was copied to the config folder", config);
     });
   }
