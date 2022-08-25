1f58d4be23804a086883d8bec41546c5c5f4d7bf
Ben Phelps <ben@phelps.io>
2022-08-25 17:57:18 +0300

    allow http proxied requests

--- src/pages/api/proxy.js
+++ src/pages/api/proxy.js
@@ -1,8 +1,31 @@
 import https from "https";
+import http from "http";
 
 function httpsRequest(params) {
   return new Promise(function (resolve, reject) {
     var request = https.request(params, function (response) {
+      let data = "";
+
+      response.on("data", (chunk) => {
+        data = data + chunk.toString();
+      });
+
+      response.on("end", () => {
+        resolve([response.statusCode, data]);
+      });
+    });
+
+    request.on("error", (error) => {
+      reject([500, error]);
+    });
+
+    request.end();
+  });
+}
+
+function httpRequest(params) {
+  return new Promise(function (resolve, reject) {
+    var request = http.request(params, function (response) {
       let data = "";
 
       response.on("data", (chunk) => {
@@ -30,23 +53,36 @@ export default async function handler(req, res) {
     return obj;
   }, {});
 
-  // this agent allows us to bypass the certificate check
-  // which is required for most self-signed certificates
-  const httpsAgent = new https.Agent({
-    rejectUnauthorized: false,
-  });
-
   const url = new URL(req.query.url);
 
-  const [status, data] = await httpsRequest({
-    hostname: url.hostname,
-    path: url.pathname,
-    port: url.port,
-    agent: httpsAgent,
-    method: req.method,
-    headers: headers,
-    body: req.method == "GET" || req.method == "HEAD" ? null : req.body,
-  });
+  if (url.protocol === "https:") {
+    // this agent allows us to bypass the certificate check
+    // which is required for most self-signed certificates
+    const httpsAgent = new https.Agent({
+      rejectUnauthorized: false,
+    });
 
-  res.status(status).send(data);
+    const [status, data] = await httpsRequest({
+      hostname: url.hostname,
+      path: url.pathname,
+      port: url.port,
+      agent: httpsAgent,
+      method: req.method,
+      headers: headers,
+      body: req.method == "GET" || req.method == "HEAD" ? null : req.body,
+    });
+
+    return res.status(status).send(data);
+  } else {
+    const [status, data] = await httpRequest({
+      hostname: url.hostname,
+      path: url.pathname,
+      port: url.port,
+      method: req.method,
+      headers: headers,
+      body: req.method == "GET" || req.method == "HEAD" ? null : req.body,
+    });
+
+    return res.status(status).send(data);
+  }
 }
