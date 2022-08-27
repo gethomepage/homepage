2faa78163a575ae507c5d2521c2c190a553efac8
AlexFullmoon <alex.fullmoon@gmail.com>
2022-08-27 14:28:47 +0300

    Cleaned up some OpenWeatherMap logic.

--- src/components/widgets/openweathermap/weather.jsx
+++ src/components/widgets/openweathermap/weather.jsx
@@ -5,7 +5,7 @@ import Icon from "./icon";
 
 export default function OpenWeatherMap({ options }) {
   const { data, error } = useSWR(
-    `/api/widgets/openweathermap?lat=${options.latitude}&lon=${options.longitude}&apiKey=${options.apiKey}&duration=${options.cache}`
+    `/api/widgets/openweathermap?lat=${options.latitude}&lon=${options.longitude}&apiKey=${options.apiKey}&duration=${options.cache}&units=${options.units}`
   );
 
   if (error) {
@@ -27,17 +27,14 @@ export default function OpenWeatherMap({ options }) {
   if (data.error) {
     return <div className="order-last grow flex-none flex flex-row items-center justify-end"></div>;
   }
-  // OpenWeatherMap returns temperature in Kelvins
-  var temp_c = data.main.temp - 273.15; 
-  var temp_f = temp_c * 9 / 5 + 32;
   return (
     <div className="order-last grow flex-none flex flex-row items-center justify-end">
       <Icon condition={data.weather[0].id} timeOfDay={(data.dt > data.sys.sunrise) && (data.dt < data.sys.sundown) ? "day" : "night"} />
       <div className="flex flex-col ml-3 text-left">
         <span className="text-theme-800 dark:text-theme-200 text-sm">
-          {options.units === "metric" ? temp_c.toFixed(0) : temp_f.toFixed(0)}&deg;
+          {data.main.temp.toFixed(1)}&deg;
         </span>
-        <span className="text-theme-800 dark:text-theme-200 text-xs">{data.weather[0].description}</span>
+        <span className="text-theme-800 dark:text-theme-200 text-xs">{data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1)}</span>
       </div>
     </div>
   );
--- src/pages/api/widgets/openweathermap.js
+++ src/pages/api/widgets/openweathermap.js
@@ -1,9 +1,9 @@
 import cachedFetch from "utils/cached-fetch";
 
 export default async function handler(req, res) {
-  const { lat, lon, apiKey, duration } = req.query;
+  const { lat, lon, apiKey, duration, units } = req.query;
 
-  const api_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  const api_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
 
   res.send(await cachedFetch(api_url, duration));
 }
