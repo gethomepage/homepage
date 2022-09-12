340b1389626ef0ef8e53a2134c8f198e925a2678
Ben Phelps <ben@phelps.io>
2022-09-12 21:13:57 +0300

    Add expanded view for resource widgets

--- src/components/widgets/resources/memory.jsx
+++ src/components/widgets/resources/memory.jsx
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next";
 
 import UsageBar from "./usage-bar";
 
-export default function Memory() {
+export default function Memory({ expanded }) {
   const { t } = useTranslation();
 
   const { data, error } = useSWR(`/api/widgets/resources?type=memory`, {
@@ -37,15 +37,27 @@ export default function Memory() {
   const percent = Math.round((data.memory.usedMemMb / data.memory.totalMemMb) * 100);
 
   return (
-    <div className="flex-none flex flex-row items-center mr-3 py-1.5 group">
+    <div className="flex-none flex flex-row items-center mr-3 py-1.5">
       <FaMemory className="text-theme-800 dark:text-theme-200 w-5 h-5" />
-      <div className="flex flex-col ml-3 text-left min-w-[80px]">
-        <span className="text-theme-800 dark:text-theme-200 text-xs group-hover:hidden">
-          {t("common.bytes", { value: data.memory.freeMemMb * 1024 * 1024 })} {t("resources.free")}
+      <div className="flex flex-col ml-3 text-left min-w-[85px]">
+        <span className="text-theme-800 dark:text-theme-200 text-xs flex flex-row justify-between">
+          <div className="pl-0.5">
+            {t("common.bytes", { value: data.memory.freeMemMb * 1024 * 1024, maximumFractionDigits: 0, binary: true })}
+          </div>
+          <div className="pr-1">{t("resources.free")}</div>
         </span>
-        <span className="text-theme-800 dark:text-theme-200 text-xs hidden group-hover:block">
-          {t("common.bytes", { value: data.memory.usedMemMb * 1024 * 1024 })} {t("resources.used")}
-        </span>
+        {expanded && (
+          <span className="text-theme-800 dark:text-theme-200 text-xs flex flex-row justify-between">
+            <div className="pl-0.5">
+              {t("common.bytes", {
+                value: data.memory.totalMemMb * 1024 * 1024,
+                maximumFractionDigits: 0,
+                binary: true,
+              })}
+            </div>
+            <div className="pr-1">{t("resources.total")}</div>
+          </span>
+        )}
         <UsageBar percent={percent} />
       </div>
     </div>
--- src/components/widgets/resources/resources.jsx
+++ src/components/widgets/resources/resources.jsx
@@ -3,14 +3,16 @@ import Cpu from "./cpu";
 import Memory from "./memory";
 
 export default function Resources({ options }) {
+  console.log(options);
+  const { expanded } = options;
   return (
     <div className="flex flex-col max-w:full sm:basis-auto self-center m-auto flex-wrap">
       <div className="flex flex-row self-center flex-wrap justify-between">
-        {options.cpu && <Cpu />}
-        {options.memory && <Memory />}
+        {options.cpu && <Cpu expanded={expanded} />}
+        {options.memory && <Memory expanded={expanded} />}
         {Array.isArray(options.disk)
-          ? options.disk.map((disk) => <Disk key={disk} options={{ disk }} />)
-          : options.disk && <Disk options={options} />}
+          ? options.disk.map((disk) => <Disk key={disk} options={{ disk }} expanded={expanded} />)
+          : options.disk && <Disk options={options} expanded={expanded} />}
       </div>
       {options.label && (
         <div className="ml-6 pt-1 text-center text-theme-800 dark:text-theme-200 text-xs">{options.label}</div>
