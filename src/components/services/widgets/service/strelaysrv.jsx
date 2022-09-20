f281d86e8afe51c744daff00e657e6a6f5a76c7e
Pablo Garcia de los Salmones Valencia <DevPGSV@gmail.com>
2022-09-20 05:29:24 +0200

    Fixed missing text

--- src/components/services/widgets/service/strelaysrv.jsx
+++ src/components/services/widgets/service/strelaysrv.jsx
@@ -31,8 +31,8 @@ export default function StRelaySrv({ service }) {
     <Widget>
       <Block label={t("strelaysrv.numActiveSessions")} value={t("common.number", { value: statsData.numActiveSessions })} />
       <Block label={t("strelaysrv.numConnections")}    value={t("common.number", { value: statsData.numConnections })} />
-      <Block label={t("strelaysrv.dataRelayed")}       value={t("common.bytes", { value: statsData.bytesProxied })} />
-      <Block label={t("strelaysrv.dataRelayed")}       value={t("common.bitrate", { value: statsData.kbps10s1m5m15m30m60m[5] })} />
+      <Block label={t("strelaysrv.dataRelayed")}       value={t("common.bytes",  { value: statsData.bytesProxied })} />
+      <Block label={t("strelaysrv.transferRate")}      value={t("common.bitrate",{ value: statsData.kbps10s1m5m15m30m60m[5] })} />
     </Widget>
   );
 }
