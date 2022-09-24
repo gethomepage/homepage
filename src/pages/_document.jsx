7709be81187d666e0d49dbdba200f7534f87b660
Ben Phelps <ben@phelps.io>
2022-09-24 14:53:46 +0300

    fix Head

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,8 +1,9 @@
-import { Html, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
+      <Head />
       <body className="relative w-full h-full bg-theme-50 dark:bg-theme-800 transition duration-150 ease-in-out">
         <Main />
         <NextScript />
