d6f6ea9dbab39fd9b638d247d8956d2c9bd80fe2
Ben Phelps <ben@phelps.io>
2022-09-25 19:43:27 +0300

    add meta tag

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta description="A highly customizable homepage (or startpage / application dashboard) with Docker and service API integrations." />
+      </Head>
       <body className="relative w-full h-full bg-theme-50 dark:bg-theme-800 transition duration-150 ease-in-out">
         <Main />
         <NextScript />
