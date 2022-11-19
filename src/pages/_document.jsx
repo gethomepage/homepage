a28253878295eaf68548967250621f9f888471b4
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-11-18 22:32:26 -0800

    Fix viewport tag location lost in merge

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,7 +9,6 @@ export default function Document() {
           content="A highly customizable homepage (or startpage / application dashboard) with Docker and service API integrations."
         />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="manifest" href="/site.webmanifest?v=4" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg?v=4" color="#1e9cd7" />
       </Head>
