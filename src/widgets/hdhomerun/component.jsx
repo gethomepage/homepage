9d5e401fe2202c0269cfe5fac3179a21afcb0530
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-11-18 22:37:28 -0800

    fix hdhomerun lint

--- src/widgets/hdhomerun/component.jsx
+++ src/widgets/hdhomerun/component.jsx
@@ -1,12 +1,8 @@
-import { useTranslation } from "next-i18next";
-
 import Container from "components/services/widget/container";
 import Block from "components/services/widget/block";
 import useWidgetAPI from "utils/proxy/use-widget-api";
 
 export default function Component({ service }) {
-  const { t } = useTranslation();
-
   const { widget } = service;
 
   const { data: channelsData, error: channelsError } = useWidgetAPI(widget, "lineup");
