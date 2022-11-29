33bd349e430e91441fc2d3a51eaeb0ae0010041e
Michael Shamoon <4887959+shamoon@users.noreply.github.com>
2022-11-28 20:10:44 -0800

    Add fallback to HTTP GET for ping

--- src/pages/api/ping.js
+++ src/pages/api/ping.js
@@ -15,11 +15,18 @@ export default async function handler(req, res) {
         });
     }
     
-    const startTime = performance.now();
-    const [status] = await httpProxy(pingURL, {
+    let startTime = performance.now();
+    let [status] = await httpProxy(pingURL, {
       method: "HEAD"
     });
-    const endTime = performance.now();
+    let endTime = performance.now();
+    
+    if (status >= 400 && status < 500) {
+      // try one more time as a GET in case HEAD is rejected for whatever reason
+      startTime = performance.now();
+      [status] = await httpProxy(pingURL);
+      endTime = performance.now();
+    }
 
     return res.status(200).json({
       status,
