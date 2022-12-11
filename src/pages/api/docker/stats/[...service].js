fb883c7b27610ccd50dfe3b31db2962b93c7e7db
Vinay Dawani <vdawani6@gmail.com>
2022-12-11 03:03:20 -0500

    added yaml file variable for swarm mode checks

--- src/pages/api/docker/stats/[...service].js
+++ src/pages/api/docker/stats/[...service].js
@@ -14,7 +14,8 @@ export default async function handler(req, res) {
   }
 
   try {
-    const docker = new Docker(getDockerArguments(containerServer));
+    const dockerArgs = getDockerArguments(containerServer)
+    const docker = new Docker(dockerArgs.conn);
     const containers = await docker.listContainers({
       all: true,
     });
@@ -41,27 +42,30 @@ export default async function handler(req, res) {
       return;
     }
 
-    // Try with a service deployed in Docker Swarm
-    const tasks = await docker.listTasks({
-      filters: {
-        service: [containerName],
-        // A service can have several offline containers, so we only look for an active one.
-        'desired-state': ['running']
+    // Try with a service deployed in Docker Swarm, if enabled
+    if (dockerArgs.swarm) {
+      const tasks = await docker.listTasks({
+          filters: {
+            service: [containerName],
+            // A service can have several offline containers, so we only look for an active one.
+            "desired-state": ["running"],
+          },
+        })
+        .catch(() => []);
+
+      // For now we are only interested in the first one (in case replicas > 1).
+      // TODO: Show the result for all replicas/containers?
+      const taskContainerId = tasks.at(0)?.Status?.ContainerStatus?.ContainerID;
+
+      if (taskContainerId) {
+        const container = docker.getContainer(taskContainerId);
+        const stats = await container.stats({ stream: false });
+
+        res.status(200).json({
+          stats,
+        });
+        return;
       }
-    }).catch(() => []);
-
-    // For now we are only interested in the first one (in case replicas > 1).
-    // TODO: Show the result for all replicas/containers?
-    const taskContainerId = tasks.at(0)?.Status?.ContainerStatus?.ContainerID
-
-    if (taskContainerId) {
-      const container = docker.getContainer(taskContainerId);
-      const stats = await container.stats({ stream: false });
-
-      res.status(200).json({
-        stats,
-      });
-      return;
     }
 
     res.status(200).send({
--- src/pages/api/docker/status/[...service].js
+++ src/pages/api/docker/status/[...service].js
@@ -13,7 +13,8 @@ export default async function handler(req, res) {
   }
 
   try {
-    const docker = new Docker(getDockerArguments(containerServer));
+    const dockerArgs = getDockerArguments(containerServer);
+    const docker = new Docker(dockerArgs.conn);
     const containers = await docker.listContainers({
       all: true,
     });
@@ -35,30 +36,33 @@ export default async function handler(req, res) {
 
       return res.status(200).json({
         status: info.State.Status,
-        health: info.State.Health?.Status
+        health: info.State.Health?.Status,
       });
     }
 
-    const tasks = await docker.listTasks({
-      filters: {
-        service: [containerName],
-        // A service can have several offline containers, we only look for an active one.
-        'desired-state': ['running']
+    if (dockerArgs.swarm) {
+      const tasks = await docker.listTasks({
+          filters: {
+            service: [containerName],
+            // A service can have several offline containers, we only look for an active one.
+            "desired-state": ["running"],
+          },
+        })
+        .catch(() => []);
+
+      // For now we are only interested in the first one (in case replicas > 1).
+      // TODO: Show the result for all replicas/containers?
+      const taskContainerId = tasks.at(0)?.Status?.ContainerStatus?.ContainerID;
+
+      if (taskContainerId) {
+        const container = docker.getContainer(taskContainerId);
+        const info = await container.inspect();
+
+        return res.status(200).json({
+          status: info.State.Status,
+          health: info.State.Health?.Status,
+        });
       }
-    }).catch(() => []);
-
-    // For now we are only interested in the first one (in case replicas > 1).
-    // TODO: Show the result for all replicas/containers?
-    const taskContainerId = tasks.at(0)?.Status?.ContainerStatus?.ContainerID
-
-    if (taskContainerId) {
-      const container = docker.getContainer(taskContainerId);
-      const info = await container.inspect();
-
-      return res.status(200).json({
-        status: info.State.Status,
-        health: info.State.Health?.Status
-      });
     }
 
     return res.status(200).send({
