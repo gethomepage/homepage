88934ec39af55c7b333baf825763f910a091cd00
shamoon <4887959+shamoon@users.noreply.github.com>
2022-12-26 06:07:43 -0800

    Correct debug messages in Pyload widget
    Closes #733

--- src/widgets/pyload/proxy.js
+++ src/widgets/pyload/proxy.js
@@ -84,9 +84,9 @@ export default async function pyloadProxyHandler(req, res) {
 
         if (data?.error || status !== 200) {
           try {
-            return res.status(status).send({error: {message: "HTTP error communicating with Plex API", data: Buffer.from(data).toString()}});
+            return res.status(status).send({error: {message: "HTTP error communicating with Pyload API", data: Buffer.from(data).toString()}});
           } catch (e) {
-            return res.status(status).send({error: {message: "HTTP error communicating with Plex API", data}});
+            return res.status(status).send({error: {message: "HTTP error communicating with Pyload API", data}});
           }
         }
 
@@ -95,7 +95,7 @@ export default async function pyloadProxyHandler(req, res) {
     }
   } catch (e) {
     logger.error(e);
-    return res.status(500).send({error: {message: `Error communicating with Plex API: ${e.toString()}`}});
+    return res.status(500).send({error: {message: `Error communicating with Pyload API: ${e.toString()}`}});
   }
 
   return res.status(400).json({ error: 'Invalid proxy service type' });
