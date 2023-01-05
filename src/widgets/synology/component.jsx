a86f832be9c02bbfad32d2854f7ba249eb3e1da2
Benoit <oupsman@oupsman.fr>
2023-01-05 18:57:23 +0100

    Synology widget : working on code stability

--- src/widgets/synology/component.jsx
+++ src/widgets/synology/component.jsx
@@ -20,7 +20,8 @@ export default function Component({ service }) {
       <Container service={service}>
         <Block label="synology.uptime" />
         <Block label="synology.volumeUsage" />
-
+        <Block label="synology.cpuLoad" />
+        <Block label="synology.memoryUsage" />
       </Container>
     );
   }
@@ -30,7 +31,8 @@ export default function Component({ service }) {
     <Container service={service}>
       <Block label="synology.uptime" value={ dsData.uptime }  />
       <Block label="synology.volumeUsage" value={t("common.percent", { value: dsData.usedVolume })} />
-
+      <Block label="synology.cpuLoad" value={t("common.percent", { value: dsData.cpuLoad })} />
+      <Block label="synology.memoryUsage" value={t("common.percent", { value: dsData.memoryUsage })} />
     </Container>
   );
 }
