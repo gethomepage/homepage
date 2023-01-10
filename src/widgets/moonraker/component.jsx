562b01e70e443307ebbc86d44988730a0631d08b
Eizock <>
2023-01-10 19:28:36 +0000

    Fix linting

--- src/widgets/moonraker/component.jsx
+++ src/widgets/moonraker/component.jsx
@@ -9,16 +9,16 @@ export default function Component({ service }) {
 
   const { widget } = service;
 
-  const { data: mr_print_stats, error: mre_print_stats } = useWidgetAPI(widget, "print_stats");
-  const { data: mr_display_status, error: mre_display_status } = useWidgetAPI(widget, "display_status");
-  const { data: mr_webhooks, error: mre_webhooks } = useWidgetAPI(widget, "webhooks");
+  const { data: printStats, error: printStatsError } = useWidgetAPI(widget, "print_stats");
+  const { data: displayStatus, error: displayStatsError } = useWidgetAPI(widget, "display_status");
+  const { data: webHooks, error: webHooksError } = useWidgetAPI(widget, "webhooks");
 
-  if (mre_print_stats || mre_display_status || mre_webhooks) {
-    const finalError = mre_print_stats ?? mre_display_status ?? mre_webhooks;
+  if (printStatsError || displayStatsError || webHooksError) {
+    const finalError = printStatsError ?? displayStatsError ?? webHooksError;
     return <Container error={finalError} />;
   }
 
-  if (!mr_print_stats || !mr_display_status || !mr_webhooks) {
+  if (!printStats || !displayStatus || !webHooks) {
     return (
       <Container service={service}>
         <div class="inline-flex flex-col" style={{width: '100%'}}>
@@ -34,44 +34,44 @@ export default function Component({ service }) {
     );
   }
 
-  const filename = "-";
-  if(!mr_print_stats.result.status.print_stats.filename == "") {
-    filename = mr_print_stats.result.status.print_stats.filename;
+  let fileName = "-";
+  if(!printStats.result.status.print_stats.filename === "") {
+    fileName = printStats.result.status.print_stats.filename;
   }
 
-  if (mr_webhooks.result.status.webhooks.state == "shutdown") {
+  if (webHooks.result.status.webhooks.state === "shutdown") {
     return (
       <Container service={service}>
         <div class="inline-flex flex-col" style={{width: '100%'}}>
-          <Block label="moonraker.printer_state" value={mr_webhooks.result.status.webhooks.state} />
+          <Block label="moonraker.printer_state" value={webHooks.result.status.webhooks.state} />
           <div class="inline-flex flex-row">
-            <Block label="moonraker.layers" value={"-"} />
-            <Block label="moonraker.print_progress" value={"-"} />
-            <Block label="moonraker.print_status" value={"-"} />
+            <Block label="moonraker.layers" value="-" />
+            <Block label="moonraker.print_progress" value="-" />
+            <Block label="moonraker.print_status" value="-" />
           </div>
-          <Block label="moonraker.filename" value={"-"} />
+          <Block label="moonraker.filename" value="-" />
         </div>
       </Container>
     );
   }
 
-  const current_layer = "-";
-  const total_layer = "-";
-  if(!mr_print_stats.result.status.print_stats.info.current_layer == "") {
-    current_layer = mr_print_stats.result.status.print_stats.info.current_layer;
-    total_layer = mr_print_stats.result.status.print_stats.info.total_layer;
+  let currentLayer = "-";
+  let totalLayer = "-";
+  if(!printStats.result.status.print_stats.info.current_layer === "") {
+    currentLayer = printStats.result.status.print_stats.info.current_layer;
+    totalLayer = printStats.result.status.print_stats.info.total_layer;
   }
 
   return (
     <Container service={service}>
       <div class="inline-flex flex-col" style={{width: '100%'}}>
-        <Block label="moonraker.printer_state" value={mr_webhooks.result.status.webhooks.state} />
+        <Block label="moonraker.printer_state" value={webHooks.result.status.webhooks.state} />
         <div class="inline-flex flex-row">
-          <Block label="moonraker.layers" value={`${current_layer} / ${total_layer}`} />
-          <Block label="moonraker.print_progress" value={t("common.percent", { value: (mr_display_status.result.status.display_status.progress * 100) })} />
-          <Block label="moonraker.print_status" value={mr_print_stats.result.status.print_stats.state} />
+          <Block label="moonraker.layers" value={`${currentLayer} / ${totalLayer}`} />
+          <Block label="moonraker.print_progress" value={t("common.percent", { value: (displayStatus.result.status.display_status.progress * 100) })} />
+          <Block label="moonraker.print_status" value={printStats.result.status.print_stats.state} />
         </div>
-        <Block label="moonraker.filename" value={filename} />
+        <Block label="moonraker.filename" value={fileName} />
       </div>
     </Container>
   );
