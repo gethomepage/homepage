19cf9969b984f14b4fbffa4dad54b63c7b79d87a
shamoon <4887959+shamoon@users.noreply.github.com>
2023-01-28 21:48:26 -0800

    Fix immich widget error catching & awaiting data

--- src/widgets/immich/component.jsx
+++ src/widgets/immich/component.jsx
@@ -5,18 +5,29 @@ import useWidgetAPI from "utils/proxy/use-widget-api";
 export default function Component({ service }) {
   const { widget } = service;
 
-  const { data: immichData } = useWidgetAPI(widget);
+  const { data: immichData, error: immichError } = useWidgetAPI(widget);
 
-  if (immichData?.statusCode) { // Unauthorized
-    return <Container error={immichData?.statusCode} />;
+  if (immichError || immichData?.statusCode === 401) {
+    return <Container error={immichError ?? immichData} />;
+  }
+
+  if (!immichData) {
+    return (
+      <Container service={service}>
+        <Block label="immich.users" />
+        <Block label="immich.photos" />
+        <Block label="immich.videos" />
+        <Block label="immich.storage" />
+      </Container>
+    );
   }
 
   return (
     <Container service={service}>
-      <Block label="immich.users" value={immichData?.usageByUser.length} />
-      <Block label="immich.photos" value={immichData?.photos} />
-      <Block label="immich.videos" value={immichData?.videos} />
-      <Block label="immich.storage" value={immichData?.usage} />
+      <Block label="immich.users" value={immichData.usageByUser.length} />
+      <Block label="immich.photos" value={immichData.photos} />
+      <Block label="immich.videos" value={immichData.videos} />
+      <Block label="immich.storage" value={immichData.usage} />
     </Container>
   );
 }
