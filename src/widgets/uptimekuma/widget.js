f35ac23fb4c9309795d0a4a3563b25ff269470cd
chain710 <chain710@gmail.com>
2023-02-01 23:22:59 +0800

    Implement uptime-kuma widget

---
+++ src/widgets/uptimekuma/widget.js
@@ -0,0 +1,8 @@
+// import credentialedProxyHandler from "utils/proxy/handlers/credentialed";
+import uptimeKumaProxyHandler from "./proxy";
+
+const widget = {
+  proxyHandler: uptimeKumaProxyHandler,
+};
+
+export default widget;
