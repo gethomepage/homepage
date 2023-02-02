c306e8442a470a362f6b9061aed2b9db76a9d2bf
shamoon <4887959+shamoon@users.noreply.github.com>
2023-02-02 00:28:18 -0800

    Simplify uptime-kuma widget

--- src/widgets/uptimekuma/component.jsx
+++ src/widgets/uptimekuma/component.jsx
@@ -4,25 +4,43 @@ import Container from "components/services/widget/container";
 import useWidgetAPI from "utils/proxy/use-widget-api";
 import Block from "components/services/widget/block";
 
-const Status = {
-  good: "uptimekuma.good",
-  warn: "uptimekuma.warn",
-  bad: "uptimekuma.bad",
-  unknown: "uptimekuma.unknown",
-};
+// Adapted from https://github.com/bastienwirtz/homer/blob/b7cd8f9482e6836a96b354b11595b03b9c3d67cd/src/components/services/UptimeKuma.vue#L59
+function getMessage(heartbeatData) {
+  let message = "good";
+  if (Object.keys(heartbeatData.heartbeatList) === 0) {
+    message = "unknown";
+  }
+
+  let hasUp = false;
+  Object.values(heartbeatData.heartbeatList).forEach((siteList) => {
+    const lastHeartbeat = siteList[siteList.length - 1];
+    if (lastHeartbeat?.status === 1) {
+      hasUp = true;
+    } else {
+      message = "warn";
+    }
+  });
+
+  if (!hasUp) {
+    message = "bad";
+  }
+
+  return message;
+}
 
 export default function Component({ service }) {
   const { t } = useTranslation();
 
   const { widget } = service;
 
-  const { data: statusData, error: statusError } = useWidgetAPI(widget);
+  const { data: statusData, error: statusError } = useWidgetAPI(widget, "status_page");
+  const { data: heartbeatData, error: heartbeatError } = useWidgetAPI(widget, "heartbeat");
 
-  if (statusError) {
-    return <Container error={statusError} />;
+  if (statusError || heartbeatError) {
+    return <Container error={statusError ?? heartbeatError} />;
   }
 
-  if (!statusData) {
+  if (!statusData || !heartbeatData) {
     return (
       <Container service={service}>
         <Block label="uptimekuma.status"/>
@@ -31,15 +49,16 @@ export default function Component({ service }) {
     );
   }
 
-  if (statusData.icon) {
-    // eslint-disable-next-line no-param-reassign
-    service.icon = statusData.icon;
-  }
+  const message = getMessage(heartbeatData);
+  // Adapted from https://github.com/bastienwirtz/homer/blob/b7cd8f9482e6836a96b354b11595b03b9c3d67cd/src/components/services/UptimeKuma.vue#L105
+  const uptimeList = Object.values(heartbeatData.uptimeList);
+  const percent = uptimeList.reduce((a, b) => a + b, 0) / uptimeList.length || 0;
+  const uptime = (percent * 100).toFixed(1);
 
   return (
     <Container service={service}>
-      <Block label="uptimekuma.status" value={statusData.incident ? statusData.incident : t(Status[statusData.message])} />
-      <Block label="uptimekuma.uptime" value={t("common.number", { value: statusData.uptime, decimals: 1 })} />
+      <Block label="uptimekuma.status" value={statusData.incident ? statusData.incident : t(`uptimekuma.${message}`)} />
+      <Block label="uptimekuma.uptime" value={t("common.percent", { value: uptime })} />
     </Container>
   );
 }
