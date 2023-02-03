f851085ebfc186b449b531fbbdc59993e09ca9e1
Bryan <32176387+KillahB33@users.noreply.github.com>
2023-02-02 22:48:40 -0500

    Komga Widget (#922)
    Co-authored-by: shamoon <4887959+shamoon@users.noreply.github.com>

---
+++ src/widgets/komga/component.jsx
@@ -0,0 +1,37 @@
+import { useTranslation } from "next-i18next";
+
+import Container from "components/services/widget/container";
+import Block from "components/services/widget/block";
+import useWidgetAPI from "utils/proxy/use-widget-api";
+
+export default function Component({ service }) {
+  const { t } = useTranslation();
+  const { widget } = service;
+
+  const { data: libraryData, error: libraryError } = useWidgetAPI(widget, "libraries");
+  const { data: seriesData, error: seriesError } = useWidgetAPI(widget, "series");
+  const { data: bookData, error: bookError } = useWidgetAPI(widget, "books");
+
+  if (libraryError || seriesError || bookError) {
+    const finalError = libraryError ?? seriesError ?? bookError;
+    return <Container error={finalError} />;
+  }
+
+  if (!libraryData || !seriesData || !bookData) {
+    return (
+      <Container service={service}>
+        <Block label="komga.libraries" />
+        <Block label="komga.series" />
+        <Block label="komga.books" />
+      </Container>
+    );
+  }
+
+  return (
+    <Container service={service}>
+      <Block label="komga.libraries" value={t("common.number", { value: libraryData.total })} />
+      <Block label="komga.series" value={t("common.number", { value: seriesData.totalElements })} />
+      <Block label="komga.books" value={t("common.number", { value: bookData.totalElements })} />
+    </Container>
+  );
+}
