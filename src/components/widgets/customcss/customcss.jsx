4b1e832c5c9e173986d39a0d58564f6754528b44
S Rei <rei@blim.de>
2023-02-15 12:05:48 +0100

    idea for customCSS loading / fixture

--- src/components/widgets/customcss/customcss.jsx
+++ src/components/widgets/customcss/customcss.jsx
@@ -1,7 +1,7 @@
 export default function Customcss({ options }) {
   if (options.src) {
     return (
-      <link href={options.src} type="text/css" rel="stylesheet"></link>
+      <link href={options.src} type="text/css" rel="stylesheet" />
     );
   }
 }
