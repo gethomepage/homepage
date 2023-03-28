e549a8771945ee5b5bc1adbf67a8711e58d71059
Sebastian Osiński <sebastian.osinski@toptal.com>
2023-03-28 19:40:09 +0200

    Display Docker container's used memory instead of total memory

--- src/widgets/docker/stats-helpers.js
+++ src/widgets/docker/stats-helpers.js
@@ -1,4 +1,4 @@
-export default function calculateCPUPercent(stats) {
+export function calculateCPUPercent(stats) {
   let cpuPercent = 0.0;
   const cpuDelta = stats.cpu_stats.cpu_usage.total_usage - stats.precpu_stats.cpu_usage.total_usage;
   const systemDelta = stats.cpu_stats.system_cpu_usage - stats.precpu_stats.system_cpu_usage;
@@ -8,4 +8,8 @@ export default function calculateCPUPercent(stats) {
   }
 
   return Math.round(cpuPercent * 10) / 10;
+}
+
+export function calculateUsedMemory(stats) {
+  return stats.memory_stats.usage - stats.memory_stats.stats.cache
 }
