489132269e9508f9434e82f96265c2834f3168fe
shamoon <4887959+shamoon@users.noreply.github.com>
2023-03-31 08:06:19 -0700

    Update [...service].js

--- src/pages/api/docker/stats/[...service].js
+++ src/pages/api/docker/stats/[...service].js
@@ -75,7 +75,7 @@ export default async function handler(req, res) {
       }
     }
 
-    return res.status(200).send({
+    return res.status(404).send({
       error: "not found",
     });
   } catch (e) {
