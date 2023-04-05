a42c50f1cc33b300b1b881f902b7c0327b7d4376
shamoon <4887959+shamoon@users.noreply.github.com>
2023-04-04 19:08:52 -0700

    fix emby widget options interpolation

--- src/utils/config/service-helpers.js
+++ src/utils/config/service-helpers.js
@@ -297,8 +297,8 @@ export function cleanServiceGroups(groups) {
           if (wan) cleanedService.widget.wan = wan;
         }
         if (type === "emby" || type === "jellyfin") {
-          if (enableBlocks) cleanedService.widget.enableBlocks = enableBlocks;
-          if (enableNowPlaying) cleanedService.widget.enableNowPlaying = enableNowPlaying;
+          if (enableBlocks !== undefined) cleanedService.widget.enableBlocks = enableBlocks;
+          if (enableNowPlaying !== undefined) cleanedService.widget.enableNowPlaying = enableNowPlaying;
         }
         if (type === "diskstation") {
           if (volume) cleanedService.widget.volume = volume;
