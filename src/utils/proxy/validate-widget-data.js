dacb0381261a1fb9de63b10abd715322b51094b4
shamoon <4887959+shamoon@users.noreply.github.com>
2023-04-11 11:15:08 -0700

    Further widget validation cleanup

--- src/utils/proxy/validate-widget-data.js
+++ src/utils/proxy/validate-widget-data.js
@@ -5,6 +5,7 @@ export default function validateWidgetData(widget, endpoint, data) {
     let valid = true;
     let dataParsed = data;
     let error;
+    let mapping;
     if (Buffer.isBuffer(data)) {
         try {
             dataParsed = JSON.parse(data);
@@ -17,7 +18,7 @@ export default function validateWidgetData(widget, endpoint, data) {
     if (dataParsed && Object.entries(dataParsed).length) {
         const mappings = widgets[widget.type]?.mappings;
         if (mappings) {
-            const mapping = Object.values(mappings).find(m => m.endpoint === endpoint);
+            mapping = Object.values(mappings).find(m => m.endpoint === endpoint);
             mapping?.validate?.forEach(key => {
                 if (dataParsed[key] === undefined) {
                     valid = false;
@@ -27,7 +28,7 @@ export default function validateWidgetData(widget, endpoint, data) {
     }
 
     if (!valid) {
-        console.warn(`Invalid data for widget '${widget.type}' endpoint '${endpoint}':\nParse error: ${error ?? "none"}\nData: ${JSON.stringify(data, null, " ")}`);
+        console.warn(`Invalid data for widget '${widget.type}' endpoint '${endpoint}':\nExpected:${mapping?.validate}\nParse error: ${error ?? "none"}\nData: ${JSON.stringify(data)}`);
     }
     
     return valid;
