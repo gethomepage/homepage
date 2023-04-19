3acbdda45efc3bd21498828cbfb6a5244a73d79d
Jon Maddox <jon@jonmaddox.com>
2023-04-19 16:24:02 -0400

    add Channels DVR Server widget

--- src/widgets/components.js
+++ src/widgets/components.js
@@ -7,6 +7,7 @@ const components = {
   autobrr: dynamic(() => import("./autobrr/component")),
   bazarr: dynamic(() => import("./bazarr/component")),
   changedetectionio: dynamic(() => import("./changedetectionio/component")),
+  channelsdvrserver: dynamic(() => import("./channelsdvrserver/component")),
   cloudflared: dynamic(() => import("./cloudflared/component")),
   coinmarketcap: dynamic(() => import("./coinmarketcap/component")),
   deluge: dynamic(() => import("./deluge/component")),
