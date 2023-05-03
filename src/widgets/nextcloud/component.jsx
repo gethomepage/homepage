d680a8656d44d3b21d1213a9cc5d5352ad3b8980
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2023-05-03 19:11:59 -0400

    Move to explicit checks

--- src/widgets/nextcloud/component.jsx
+++ src/widgets/nextcloud/component.jsx
@@ -14,19 +14,21 @@ export default function Component({ service }) {
     return <Container service={service} error={nextcloudError} />;
   }
   
-  // cpuload & memoryusage were deprecated, dont break existing installs & dont have > 4 blocks total
-  let deprecatedFieldsCount = widget.fields ? widget.fields.includes('cpuload') + widget.fields.includes('memoryusage') : 0;
-  if (widget.fields && widget.fields.length - deprecatedFieldsCount < 4) deprecatedFieldsCount -= 4 - (widget.fields.length - deprecatedFieldsCount);
+  // cpuload & memoryusage are deprecated, so limit to 4 fields
+  const showCpuLoad = widget.fields?.includes('cpuload');
+  const showMemoryUsage = widget.fields?.includes('memoryusage');
+  const showNumFiles = !showCpuLoad || !showMemoryUsage; // at least 1 deprecated field is hidden
+  const showNumShares = !showCpuLoad && !showMemoryUsage; // both deprecated fields are hidden
 
   if (!nextcloudData) {
     return (
       <Container service={service}>
-        {widget.fields?.includes('cpuload') && <Block label="nextcloud.cpuload" />}
-        {widget.fields?.includes('memoryusage') && <Block label="nextcloud.memoryusage" />}
+        {showCpuLoad && <Block label="nextcloud.cpuload" />}
+        {showMemoryUsage && <Block label="nextcloud.memoryusage" />}
         <Block label="nextcloud.freespace" />
         <Block label="nextcloud.activeusers" />
-        {deprecatedFieldsCount < 2 && <Block label="nextcloud.numfiles" />}
-        {deprecatedFieldsCount < 1 && <Block label="nextcloud.numshares" />}
+        {showNumFiles && <Block label="nextcloud.numfiles" />}
+        {showNumShares && <Block label="nextcloud.numshares" />}
       </Container>
     );
   }
@@ -36,12 +38,12 @@ export default function Component({ service }) {
 
   return (
     <Container service={service}>
-      {widget.fields?.includes('cpuload') && <Block label="nextcloud.cpuload" value={t("common.percent", { value: nextcloudInfo.system.cpuload[0] })} />}
-      {widget.fields?.includes('memoryusage') && <Block label="nextcloud.memoryusage" value={t("common.percent", { value:memoryUsage })} />}
+      {showCpuLoad && <Block label="nextcloud.cpuload" value={t("common.percent", { value: nextcloudInfo.system.cpuload[0] })} />}
+      {showMemoryUsage && <Block label="nextcloud.memoryusage" value={t("common.percent", { value:memoryUsage })} />}
       <Block label="nextcloud.freespace" value={t("common.bbytes", { value: nextcloudInfo.system.freespace, maximumFractionDigits: 1 })} />
       <Block label="nextcloud.activeusers" value={t("common.number", { value: nextcloudData.ocs.data.activeUsers.last24hours })} />
-      {deprecatedFieldsCount < 2 && <Block label="nextcloud.numfiles" value={t("common.number", { value: nextcloudInfo.storage.num_files })} />}
-      {deprecatedFieldsCount < 1 && <Block label="nextcloud.numshares" value={t("common.number", { value: nextcloudInfo.shares.num_shares })} />}
+      {showNumFiles && <Block label="nextcloud.numfiles" value={t("common.number", { value: nextcloudInfo.storage.num_files })} />}
+      {showNumShares && <Block label="nextcloud.numshares" value={t("common.number", { value: nextcloudInfo.shares.num_shares })} />}
     </Container>
   );
 }
