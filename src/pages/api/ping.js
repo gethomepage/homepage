f1a33f10edaa199c37a0bfce65562d84c838c9a6
shamoon <4887959+shamoon@users.noreply.github.com>
2023-05-20 09:48:18 -0700

    Add try catch for ping

--- src/pages/api/ping.js
+++ src/pages/api/ping.js
@@ -14,22 +14,29 @@ export default async function handler(req, res) {
         error: "No ping URL given",
         });
     }
-    
-    let startTime = performance.now();
-    let [status] = await httpProxy(pingURL, {
-      method: "HEAD"
-    });
-    let endTime = performance.now();
-    
-    if (status > 403) {
-      // try one more time as a GET in case HEAD is rejected for whatever reason
-      startTime = performance.now();
-      [status] = await httpProxy(pingURL);
-      endTime = performance.now();
-    }
 
-    return res.status(200).json({
-      status,
-      latency: endTime - startTime
-    });
+    try {
+      let startTime = performance.now();
+      let [status] = await httpProxy(pingURL, {
+        method: "HEAD"
+      });
+      let endTime = performance.now();
+      
+      if (status > 403) {
+        // try one more time as a GET in case HEAD is rejected for whatever reason
+        startTime = performance.now();
+        [status] = await httpProxy(pingURL);
+        endTime = performance.now();
+      }
+  
+      return res.status(200).json({
+        status,
+        latency: endTime - startTime
+      });
+    } catch (e) {
+      logger.debug("Error attempting ping: %s", JSON.stringify(e));
+      return res.status(400).send({
+        error: 'Error attempting ping, see logs.',
+      });
+    }
 }
