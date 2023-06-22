b32d61053274dd1f639103f6eb8959e9e5775e34
shamoon <4887959+shamoon@users.noreply.github.com>
2023-06-22 11:51:38 -0700

    Adds collapsible feature to bookmarks

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -308,7 +308,10 @@ function Home({ initialSettings }) {
         {bookmarks?.length > 0 && (
           <div className={`grow flex flex-wrap pt-0 p-4 sm:p-8 gap-2 grid-cols-1 lg:grid-cols-2 lg:grid-cols-${Math.min(6, bookmarks.length)}`}>
             {bookmarks.map((group) => (
-              <BookmarksGroup key={group.name} group={group} />
+              <BookmarksGroup
+                key={group.name}
+                group={group}
+                disableCollapse={settings.disableCollapse} />
             ))}
           </div>
         )}
