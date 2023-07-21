84d7341e2bd52302558efc37b70bfb99d5b9c584
Nitzan Miranda <Nizzan943@gmail.com>
2023-07-21 04:07:10 +0300

    add creatorId

--- src/widgets/azurePullrequests/widget.js
+++ src/widgets/azurePullrequests/widget.js
@@ -1,14 +1,25 @@
 import credentialedProxyHandler from "utils/proxy/handlers/credentialed";
+import { asJson } from "utils/proxy/api-helpers";
 
 const widget = {
-  api: "https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repositoryId}/pullrequests",
+  api: "https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repositoryId}/pullrequests?{filter}",
   proxyHandler: credentialedProxyHandler,
 
-  active: {
+  mappings: {
     pr: {
-      endpoint: "pr",
-    }
+      filter: "",
+      map: (data) => ({
+        count: asJson(data).count,
+      }),
+    },
+    myPr: {
+      filter: "searchCriteria.creatorId={creatorId}&",
+      map: (data) => ({
+        count: asJson(data).count,
+      }),
+    },
   },
 };
+
 
 export default widget;
