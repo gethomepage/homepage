e006bf489f92bffaff45990c18266b800edfab43
Nitzan Miranda <Nizzan943@gmail.com>
2023-07-21 19:42:12 +0300

    remove unused code

--- src/widgets/azurePullrequests/widget.js
+++ src/widgets/azurePullrequests/widget.js
@@ -5,11 +5,4 @@ const widget = {
   proxyHandler: credentialedProxyHandler,
 };
 
-// mappings: {
-//   "v1/cryptocurrency/quotes/latest": {
-//     endpoint: "v1/cryptocurrency/quotes/latest",
-//     params: ["convert"],
-//     optionalParams: ["symbol", "slug"],
-//   },
-
 export default widget;
