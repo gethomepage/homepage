09d44da0871ae9548ce8266a559f464f1ec5f113
Ben Phelps <ben@phelps.io>
2023-08-02 06:04:20 +0300

    add effect overlay target

--- src/widgets/glances/components/container.jsx
+++ src/widgets/glances/components/container.jsx
@@ -1,7 +1,8 @@
-export default function Container({ children }) {
+export default function Container({ children, className = "" }) {
   return (
     <div>
       {children}
+      <div className={`absolute top-0 right-0 bottom-0 left-0 overflow-clip pointer-events-none ${className}`} />
       <div className="h-[68px] overflow-clip" />
     </div>
   );
