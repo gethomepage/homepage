8368b531852ed332eb9c103b42834d9ef5e1f452
Xinos <userxinos@gmail.com>
2023-08-16 15:19:44 +0700

    Zfs getStats method

--- src/widgets/openmediavault/component.jsx
+++ src/widgets/openmediavault/component.jsx
@@ -1,6 +1,7 @@
 import ServicesGetStatus from "./methods/services_get_status";
 import SmartGetList from "./methods/smart_get_list";
 import DownloaderGetDownloadList from "./methods/downloader_get_downloadlist";
+import ZfsGetStats from "./methods/zfs_get_stats";
 
 export default function Component({ service }) {
   switch (service.widget.method) {
@@ -10,6 +11,8 @@ export default function Component({ service }) {
       return <SmartGetList service={service} />;
     case "downloader.getDownloadList":
       return <DownloaderGetDownloadList service={service} />;
+    case "zfs.getStats":
+      return <ZfsGetStats service={service} />;
     default:
       return null;
   }
