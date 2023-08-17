0ec6393e0aec304df3d91646df7c3f14b351ebb5
Xinos <userxinos@gmail.com>
2023-08-17 15:17:02 +0700

    Revert "Zfs getStats method"
    This reverts commit 8368b531852ed332eb9c103b42834d9ef5e1f452.

--- src/widgets/openmediavault/component.jsx
+++ src/widgets/openmediavault/component.jsx
@@ -1,7 +1,6 @@
 import ServicesGetStatus from "./methods/services_get_status";
 import SmartGetList from "./methods/smart_get_list";
 import DownloaderGetDownloadList from "./methods/downloader_get_downloadlist";
-import ZfsGetStats from "./methods/zfs_get_stats";
 
 export default function Component({ service }) {
   switch (service.widget.method) {
@@ -11,8 +10,6 @@ export default function Component({ service }) {
       return <SmartGetList service={service} />;
     case "downloader.getDownloadList":
       return <DownloaderGetDownloadList service={service} />;
-    case "zfs.getStats":
-      return <ZfsGetStats service={service} />;
     default:
       return null;
   }
