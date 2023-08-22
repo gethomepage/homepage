eb67cf1d6ff3645b547570fd50ad76f3be14cc94
shamoon <4887959+shamoon@users.noreply.github.com>
2023-08-21 19:31:48 -0700

    Revert "Calibre web widget (#1829)"
    This reverts commit 4c49767a0fd4df8f212efacc803d9b3b1601bf71.

--- src/widgets/widgets.js
+++ src/widgets/widgets.js
@@ -6,7 +6,6 @@ import autobrr from "./autobrr/widget";
 import azuredevops from "./azuredevops/widget";
 import bazarr from "./bazarr/widget";
 import caddy from "./caddy/widget";
-import calibreweb from "./calibreweb/widget";
 import changedetectionio from "./changedetectionio/widget";
 import channelsdvrserver from "./channelsdvrserver/widget";
 import cloudflared from "./cloudflared/widget";
@@ -104,7 +103,6 @@ const widgets = {
   azuredevops,
   bazarr,
   caddy,
-  calibreweb,
   changedetectionio,
   channelsdvrserver,
   cloudflared,
