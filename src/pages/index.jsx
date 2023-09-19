78d9df1fa83b468b8e4f803bb0bc8612342c2628
shamoon <4887959+shamoon@users.noreply.github.com>
2023-09-18 21:34:18 -0700

    Fix custom.js not working

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -362,13 +362,7 @@ function Home({ initialSettings }) {
         />
       </style>
       <link rel="preload" href="/api/config/custom.js" as="fetch" crossOrigin="anonymous" />
-      <script data-name="custom.js">
-        <FileContent path="custom.js"
-          loadingValue="/* Loading custom JS... */"
-          errorValue="/* Failed to load custom JS... */"
-          emptyValue="/* No custom JS */"
-        />
-      </script>
+      <script data-name="custom.js" src="/api/config/custom.js" async="{true}" />
 
       <div className="relative container m-auto flex flex-col justify-start z-10 h-full">
         <QuickLaunch
