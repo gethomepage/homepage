017ed7932e7b342c03627b3eb69a198e967feb66
shamoon <4887959+shamoon@users.noreply.github.com>
2023-09-27 21:36:29 -0700

    Translate radarr release strings

--- src/widgets/calendar/integrations/radarr.jsx
+++ src/widgets/calendar/integrations/radarr.jsx
@@ -1,11 +1,13 @@
 import { DateTime } from "luxon";
 import { useEffect, useContext } from "react";
+import { useTranslation } from "next-i18next";
 
 import useWidgetAPI from "../../../utils/proxy/use-widget-api";
 import { EventContext } from "../../../utils/contexts/calendar";
 import Error from "../../../components/services/widget/error";
 
 export default function Integration({ config, params }) {
+  const { t } = useTranslation();
   const { setEvents } = useContext(EventContext);
   const { data: radarrData, error: radarrError } = useWidgetAPI(config, "calendar",
     { ...params,  ...config?.params ?? {} }
@@ -18,9 +20,9 @@ export default function Integration({ config, params }) {
     const eventsToAdd = {};
 
     radarrData?.forEach(event => {
-      const cinemaTitle = `${event.title} - In cinemas`;
-      const physicalTitle = `${event.title} - Physical release`;
-      const digitalTitle = `${event.title} - Digital release`;
+      const cinemaTitle = `${event.title} - ${t("calendar.inCinemas")}`;
+      const physicalTitle = `${event.title} - ${t("calendar.physicalRelease")}`;
+      const digitalTitle = `${event.title} - ${t("calendar.digitalRelease")}`;
 
       eventsToAdd[cinemaTitle] = {
         title: cinemaTitle,
@@ -40,7 +42,7 @@ export default function Integration({ config, params }) {
     })
 
     setEvents((prevEvents) => ({ ...prevEvents, ...eventsToAdd }));
-  }, [radarrData, radarrError, config, setEvents]);
+  }, [radarrData, radarrError, config, setEvents, t]);
 
   const error = radarrError ?? radarrData?.error;
   return error && <Error error={{ message: `${config.type}: ${error.message ?? error}`}} />
