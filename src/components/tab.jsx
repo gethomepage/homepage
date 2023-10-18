b31dd8a1be52bdca471241c0f6a44ddc223dba1e
shamoon <4887959+shamoon@users.noreply.github.com>
2023-10-18 13:22:05 -0700

    Fix tab spacing on mobile

--- src/components/tab.jsx
+++ src/components/tab.jsx
@@ -14,7 +14,7 @@ export default function Tab({ tab }) {
     <li
       key={tab}
       role="presentation"
-      className={classNames("text-theme-700 dark:text-theme-200 relative h-8 w-full rounded-md flex m-1")}
+      className={classNames("text-theme-700 dark:text-theme-200 relative h-10 w-full rounded-md flex")}
     >
       <button
         id={`${tab}-tab`}
@@ -23,7 +23,7 @@ export default function Tab({ tab }) {
         aria-controls={`#${tab}`}
         aria-selected={activeTab === slugify(tab) ? "true" : "false"}
         className={classNames(
-          "h-full w-full rounded-md",
+          "w-full rounded-md m-1",
           activeTab === slugify(tab)
             ? "bg-theme-300/20 dark:bg-white/10"
             : "hover:bg-theme-100/20 dark:hover:bg-white/5",
