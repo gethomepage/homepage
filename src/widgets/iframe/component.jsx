6d1478ea1f1b1feb87fe1dcc6982c4847a7ecc0c
Reiss Cashmore <reiss.cashmore@outlook.com>
2023-10-30 19:23:51 +0000

    Linting

--- src/widgets/iframe/component.jsx
+++ src/widgets/iframe/component.jsx
@@ -12,26 +12,21 @@ export default function Component({ service }) {
     if (widget?.refreshInterval) {
       setInterval(
         () => setRefreshTimer(refreshTimer + 1),
-        widget?.refreshInterval < 1000 ? 1000 : widget?.refreshInterval
+        widget?.refreshInterval < 1000 ? 1000 : widget?.refreshInterval,
       );
     }
   }, [refreshTimer, widget?.refreshInterval]);
 
-  const scrollingDisableStyle =
-    widget?.allowScrolling === "no"
-      ? { pointerEvents: "none", overflow: "hidden" }
-      : {};
+  const scrollingDisableStyle = widget?.allowScrolling === "no" ? { pointerEvents: "none", overflow: "hidden" } : {};
 
-  const classes =
-    widget?.classes ||
-    "h-60 sm:h-60 md:h-60 lg:h-60 xl:h-60 2xl:h-72 w-full rounded";
+  const classes = widget?.classes || "h-60 sm:h-60 md:h-60 lg:h-60 xl:h-60 2xl:h-72 w-full rounded";
 
   return (
     <Container service={service}>
       <div
         className={classNames(
           "bg-theme-200/50 dark:bg-theme-900/20 rounded m-1 flex-1 flex flex-col items-center justify-center text-center",
-          "service-block"
+          "service-block",
         )}
       >
         <iframe
