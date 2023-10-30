3f8944c878faa99cdf447878b5baa40209e6883e
Reiss Cashmore <reiss.cashmore@outlook.com>
2023-10-30 17:28:39 +0000

    Remove padding

--- src/widgets/iframe/component.jsx
+++ src/widgets/iframe/component.jsx
@@ -27,7 +27,7 @@ export default function Component({ service }) {
     <Container service={service}>
       <div
         className={classNames(
-          "bg-theme-200/50 dark:bg-theme-900/20 rounded m-1 flex-1 flex flex-col items-center justify-center text-center p-1",
+          "bg-theme-200/50 dark:bg-theme-900/20 rounded m-1 flex-1 flex flex-col items-center justify-center text-center",
           "service-block",
         )}
       >
