5aa7a8de964b6c2e7c33c70cf9b201cef3f14baf
shamoon <4887959+shamoon@users.noreply.github.com>
2023-10-30 08:03:28 -0700

    Update component.jsx

--- src/widgets/iframe/component.jsx
+++ src/widgets/iframe/component.jsx
@@ -25,7 +25,12 @@ export default function Component({ service }) {
 
   return (
     <Container service={service}>
-      <Block>
+      <div
+        className={classNames(
+          "bg-theme-200/50 dark:bg-theme-900/20 rounded m-1 flex-1 flex flex-col items-center justify-center text-center p-1",
+          "service-block",
+        )}
+      >
         <iframe
           src={widget?.src}
           key={`${widget?.name}-${refreshTimer}`}
@@ -40,20 +45,7 @@ export default function Component({ service }) {
           style={{ width: "100%", scrollingDisableStyle }}
           className={sizeClasses}
         />
-      </Block>
+      </div>
     </Container>
-  );
-}
-
-function Block({ children }) {
-  return (
-    <div
-      className={classNames(
-        "bg-theme-200/50 dark:bg-theme-900/20 rounded m-1 flex-1 flex flex-col items-center justify-center text-center p-1",
-        "service-block",
-      )}
-    >
-      {children}
-    </div>
   );
 }
