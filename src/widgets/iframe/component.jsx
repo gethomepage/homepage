74a194fe53f0a5dd6d9444c9d6228a4f7746be7f
Reiss Cashmore <reiss.cashmore@outlook.com>
2023-10-30 14:39:31 +0000

    Refactor block component

--- src/widgets/iframe/component.jsx
+++ src/widgets/iframe/component.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
-
-import Block from "./components/block";
+import classNames from "classnames";
 
 import Container from "components/services/widget/container";
 
@@ -51,5 +50,19 @@ export default function Component({ service }) {
         />
       </Block>
     </Container>
+  );
+}
+
+
+function Block({ children }) {
+  return (
+    <div
+      className={classNames(
+        "bg-theme-200/50 dark:bg-theme-900/20 rounded m-1 flex-1 flex flex-col items-center justify-center text-center p-1",
+        "service-block"
+      )}
+    >
+      {children}
+    </div>
   );
 }
