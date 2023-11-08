03255eead8ec6d1a77fff5ddf809f281aa1f60ef
Gabe Cook <gabe565@gmail.com>
2023-11-08 00:17:50 -0600

    Fix: Update Immich widget URL for Immich v1.85.0+

--- src/widgets/immich/widget.js
+++ src/widgets/immich/widget.js
@@ -1,7 +1,7 @@
 import credentialedProxyHandler from "utils/proxy/handlers/credentialed";
 
 const widget = {
-  api: "{url}/api/server-info/stats",
+  api: "{url}/api/server-info/statistics",
   proxyHandler: credentialedProxyHandler,
 };
 
