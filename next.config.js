09be472463622686da7fdbb26987fa254fc99139
shamoon <4887959+shamoon@users.noreply.github.com>
2023-11-11 21:05:25 -0800

    dont disable swc

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
-  swcMinify: false,
   images: {
     domains: ["cdn.jsdelivr.net"],
     unoptimized: true,
