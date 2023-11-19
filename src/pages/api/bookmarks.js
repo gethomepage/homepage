19d781706b80fbdf0bd897fcfc65ebf933064cec
Aaron Dalton <aadalton@vt.edu>
2023-11-18 23:18:05 -0500

    Add group authentication and documentation

--- src/pages/api/bookmarks.js
+++ src/pages/api/bookmarks.js
@@ -1,8 +1,8 @@
-import { createAuthProvider } from "utils/auth/auth-helpers";
+import { readAuthSettings } from "utils/auth/auth-helpers";
 import { bookmarksResponse } from "utils/config/api-response";
 import { getSettings } from "utils/config/config";
 
 export default async function handler(req, res) {
-  const auth = createAuthProvider(getSettings()) 
-  res.send(await bookmarksResponse(auth.permissions(req)));
+  const { provider, groups } = readAuthSettings(getSettings().auth)
+  res.send(await bookmarksResponse(provider.permissions(req), groups));
 }
