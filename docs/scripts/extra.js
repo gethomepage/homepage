507e72407f4404b06aa0cd5af7b35eebe2e3e339
shamoon <4887959+shamoon@users.noreply.github.com>
2023-11-25 08:37:02 -0800

    Add AI bot info to docs

--- docs/scripts/extra.js
+++ docs/scripts/extra.js
@@ -1,6 +1,12 @@
+var glimeScript;
 document$.subscribe(function () {
-  var glimeScript = document.createElement("script");
-  glimeScript.setAttribute("src", "https://cdn.glimelab.ai/widget/1.0.0/widget.js");
-  glimeScript.setAttribute("onload", 'window.glime.init("Bl3mlvfCnTnRm5");');
-  document.head.appendChild(glimeScript);
+  if (!glimeScript) {
+    glimeScript = document.createElement("script");
+    glimeScript.setAttribute("data", "name: 'glime'");
+    glimeScript.setAttribute("src", "https://cdn.glimelab.ai/widget/1.0.0/widget.js");
+    glimeScript.setAttribute("onload", 'window.glime.init("Bl3mlvfCnTnRm5");');
+    document.head.appendChild(glimeScript);
+  } else {
+    // window.glime.init("Bl3mlvfCnTnRm5");
+  }
 });
