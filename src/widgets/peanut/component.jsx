fb958565bc32778aeac6cd40e8ad171588131718
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2023-12-09 16:45:53 -0500

    fix lint

--- src/widgets/peanut/component.jsx
+++ src/widgets/peanut/component.jsx
@@ -4,7 +4,6 @@ import Container from "components/services/widget/container";
 import Block from "components/services/widget/block";
 import useWidgetAPI from "utils/proxy/use-widget-api";
 
-
 export default function Component({ service }) {
   const { widget } = service;
   const { t } = useTranslation();
