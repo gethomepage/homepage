4cfe97c0267e2dd7ffd6a760d2b0674ee13dbfcf
shamoon <4887959+shamoon@users.noreply.github.com>
2023-12-15 14:03:47 -0800

    Unify uptime formatting

--- src/components/widgets/resources/uptime.jsx
+++ src/components/widgets/resources/uptime.jsx
@@ -20,17 +20,14 @@ export default function Uptime({ refresh = 1500 }) {
     return <Resource icon={FaRegClock} value="-" label={t("resources.uptime")} percentage="0" />;
   }
 
-  const mo = Math.floor(data.uptime / (3600 * 24 * 31));
-  const d = Math.floor((data.uptime % (3600 * 24 * 31)) / (3600 * 24));
-  const h = Math.floor((data.uptime % (3600 * 24)) / 3600);
-  const m = Math.floor((data.uptime % 3600) / 60);
-
-  let uptime;
-  if (mo > 0) uptime = `${mo}${t("resources.months")} ${d}${t("resources.days")}`;
-  else if (d > 0) uptime = `${d}${t("resources.days")} ${h}${t("resources.hours")}`;
-  else uptime = `${h}${t("resources.hours")} ${m}${t("resources.minutes")}`;
-
   const percent = Math.round((new Date().getSeconds() / 60) * 100).toString();
 
-  return <Resource icon={FaRegClock} value={uptime} label={t("resources.uptime")} percentage={percent} />;
+  return (
+    <Resource
+      icon={FaRegClock}
+      value={t("common.uptime", { value: data.uptime })}
+      label={t("resources.uptime")}
+      percentage={percent}
+    />
+  );
 }
