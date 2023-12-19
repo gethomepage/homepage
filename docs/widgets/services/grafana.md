ee2eb7abe1ca95e9a3b153e8a5edc2b8cc4a3bef
myztillx <samuel.geist@gmail.com>
2023-12-19 11:11:06 -0500

    Added option for grafana alertmanager

--- docs/widgets/services/grafana.md
+++ docs/widgets/services/grafana.md
@@ -3,7 +3,9 @@ title: Grafana
 description: Grafana Widget Configuration
 ---
 
-Allowed fields: `["dashboards", "datasources", "totalalerts", "alertstriggered"]`.
+Allowed fields: `["dashboards", "datasources", "totalalerts", "alertstriggered", "alertmanager"]`.
+
+`alertmanager` is for the new grafana alerts api since the api used by `alertstriggered` has been deprecated. If `alertstriggered` does not work, try `alertmanager`.
 
 ```yaml
 widget:
--- src/widgets/grafana/component.jsx
+++ src/widgets/grafana/component.jsx
@@ -10,18 +10,20 @@ export default function Component({ service }) {
   const { widget } = service;
   const { data: statsData, error: statsError } = useWidgetAPI(widget, "stats");
   const { data: alertsData, error: alertsError } = useWidgetAPI(widget, "alerts");
+  const { data: alertmanagerData, error: alertmanagerError } = useWidgetAPI(widget, "alertmanager");
 
-  if (statsError || alertsError) {
+  if (statsError || alertsError || alertmanagerError) {
     return <Container service={service} error={statsError ?? alertsError} />;
   }
 
-  if (!statsData || !alertsData) {
+  if (!statsData || !alertsData || !alertmanagerData) {
     return (
       <Container service={service}>
         <Block label="grafana.dashboards" />
         <Block label="grafana.datasources" />
         <Block label="grafana.totalalerts" />
         <Block label="grafana.alertstriggered" />
+        <Block label="grafana.alertmanager" />
       </Container>
     );
   }
@@ -34,6 +36,10 @@ export default function Component({ service }) {
       <Block
         label="grafana.alertstriggered"
         value={t("common.number", { value: alertsData.filter((a) => a.state === "alerting").length })}
+      />
+      <Block
+        label="grafana.alertmanager"
+        value={t("common.number", { value: alertmanagerData.length })}
       />
     </Container>
   );
