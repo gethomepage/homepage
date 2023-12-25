d54e1d0879e10f8b4bd6294ebc2cb5cfaa237ba5
Aaron Dalton <aadalton@vt.edu>
2023-12-25 12:05:54 -0500

    Fix precommit isssues

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -2,16 +2,16 @@ import { checkAllowedGroup, readAuthSettings } from "utils/auth/auth-helpers";
 import { getSettings } from "utils/config/config";
 
 export default async function handler(req, res) {
-    const { group } = req.query;
-    const { provider, groups } = readAuthSettings(getSettings().auth)
+  const { group } = req.query;
+  const { provider, groups } = readAuthSettings(getSettings().auth);
 
-    try {
-        if (checkAllowedGroup(provider.permissions(req), groups, group)) {
-            res.json({group})
-        } else {
-            res.status(401).json({message:"Group unathorized"})
-        }
-    } catch (err) {
-        res.status(500).send("Error authenticating");
+  try {
+    if (checkAllowedGroup(provider.permissions(req), groups, group)) {
+      res.json({ group });
+    } else {
+      res.status(401).json({ message: "Group unathorized" });
     }
+  } catch (err) {
+    res.status(500).send("Error authenticating");
+  }
 }
--- src/utils/auth/null.js
+++ src/utils/auth/null.js
@@ -1,23 +1,23 @@
-const NullPermissions = { user: null, groups:[]}
-const NullAuthKey = "none"
+const NullPermissions = { user: null, groups: [] };
+const NullAuthKey = "none";
 
 function createNullAuth() {
-    return {
-        authorize: () => NullPermissions,
-        getContext: () => ({
-            provider: NullAuthKey
-        }), 
-    }
-} 
+  return {
+    authorize: () => NullPermissions,
+    getContext: () => ({
+      provider: NullAuthKey,
+    }),
+  };
+}
 
 async function fetchNullAuth([key]) {
-    return fetch(key).then((res) => res.json())
+  return fetch(key).then((res) => res.json());
 }
 
 const NullAuthProvider = {
-    key: NullAuthKey, 
-    create: createNullAuth, 
-    fetch: fetchNullAuth
-}
+  key: NullAuthKey,
+  create: createNullAuth,
+  fetch: fetchNullAuth,
+};
 
 export default NullAuthProvider;
