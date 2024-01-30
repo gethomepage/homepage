a7582c4d92e229f5a0e3969af30b441364806ece
Derek Stotz <okiedokieiguess@gmail.com>
2024-01-29 18:30:38 -0600

    Add pools option to TrueNAS service widget

--- docs/widgets/services/truenas.md
+++ docs/widgets/services/truenas.md
@@ -9,6 +9,8 @@ Allowed fields: `["load", "uptime", "alerts"]`.
 
 To create an API Key, follow [the official TrueNAS documentation](https://www.truenas.com/docs/scale/scaletutorials/toptoolbar/managingapikeys/).
 
+A detailed pool listing is disabled by default, but can be enabled with the `enablePools` option.
+
 ```yaml
 widget:
   type: truenas
@@ -16,4 +18,5 @@ widget:
   username: user # not required if using api key
   password: pass # not required if using api key
   key: yourtruenasapikey # not required if using username / password
+  enablePools: true # optional, defaults to false
 ```
--- src/widgets/truenas/widget.js
+++ src/widgets/truenas/widget.js
@@ -1,32 +1,10 @@
-import { jsonArrayFilter } from "utils/proxy/api-helpers";
-import credentialedProxyHandler from "utils/proxy/handlers/credentialed";
-import genericProxyHandler from "utils/proxy/handlers/generic";
-import getServiceWidget from "utils/config/service-helpers";
+import truenasProxyHandler from "./proxy";
+
+import { asJson, jsonArrayFilter } from "utils/proxy/api-helpers";
 
 const widget = {
   api: "{url}/api/v2.0/{endpoint}",
-  proxyHandler: async (req, res, map) => {
-    // choose proxy handler based on widget settings
-    const { group, service } = req.query;
-
-    if (group && service) {
-      const widgetOpts = await getServiceWidget(group, service);
-      let handler;
-      if (widgetOpts.username && widgetOpts.password) {
-        handler = genericProxyHandler;
-      } else if (widgetOpts.key) {
-        handler = credentialedProxyHandler;
-      }
-
-      if (handler) {
-        return handler(req, res, map);
-      }
-
-      return res.status(500).json({ error: "Username / password or API key required" });
-    }
-
-    return res.status(500).json({ error: "Error parsing widget request" });
-  },
+  proxyHandler: truenasProxyHandler,
 
   mappings: {
     alerts: {
@@ -38,6 +16,19 @@ const widget = {
     status: {
       endpoint: "system/info",
       validate: ["loadavg", "uptime_seconds"],
+    },
+    pools: {
+      endpoint: "pool",
+      map: (data) =>
+        asJson(data).map((entry) => ({
+          id: entry.name,
+          name: entry.name,
+          status: entry.status,
+          healthy: entry.healthy,
+          used: entry.allocated,
+          size: entry.size,
+          free: entry.free,
+        })),
     },
   },
 };
