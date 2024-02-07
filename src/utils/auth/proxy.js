be15fca286a1a65d2b6907fd642fd37100ae6f92
Aaron Dalton <zenoftheblues@gmail.com>
2024-02-07 09:14:17 -0500

    Fix property accessors

--- src/utils/auth/proxy.js
+++ src/utils/auth/proxy.js
@@ -1,8 +1,8 @@
 // 'proxy' auth provider is meant to be used by a reverse proxy that injects permission headers into the origin
 // request. In this case we are relying on our proxy to authenitcate our users and validate.
 function getProxyPermissions(userHeader, groupHeader, request) {
-  const user = userHeader ? request.headers.get(userHeader) : null;
-  const groupsString = groupHeader ? request.headers.get(groupHeader) : "";
+  const user = userHeader ? request.headers[userHeader] : null;
+  const groupsString = groupHeader ? request.headers[groupHeader] : "";
 
   return { user, groups: groupsString ? groupsString.split(",").map((v) => v.trimStart()) : [] };
 }
@@ -11,8 +11,8 @@ function createProxyAuth({ groupHeader, userHeader }) {
   return {
     getContext: (request) => ({
       provider: "proxy",
-      ...(userHeader && { [userHeader]: request.headers.get(userHeader) }),
-      ...(groupHeader && { [groupHeader]: request.headers.get(groupHeader) }),
+      ...(userHeader && { [userHeader]: request.headers[userHeader] }),
+      ...(groupHeader && { [groupHeader]: request.headers[groupHeader] }),
     }),
     authorize: (request) => getProxyPermissions(userHeader, groupHeader, request),
   };
