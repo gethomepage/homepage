9a7650d695c38156dc4c02d9715aff624beeb9d4
Flo2410 <florian@hye.dev>
2024-02-09 19:20:12 +0000

    Move install of python requirements into setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,6 +3,8 @@
 # Install Node packages
 pnpm install
 
+python3 -m pip install -r requirements.txt
+
 # Copy in skeleton configuration if there is no existing configuration
 if [ ! -d "config/" ]; then
   echo "Adding skeleton config"
