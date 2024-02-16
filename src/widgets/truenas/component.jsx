8e1474a760d69fe23fadc1ad991bb232c7c01696
shamoon <4887959+shamoon@users.noreply.github.com>
2024-02-15 21:24:54 -0800

    Move truenas pool component

--- src/widgets/truenas/component.jsx
+++ src/widgets/truenas/component.jsx
@@ -3,7 +3,7 @@ import { useTranslation } from "next-i18next";
 import Container from "components/services/widget/container";
 import Block from "components/services/widget/block";
 import useWidgetAPI from "utils/proxy/use-widget-api";
-import Pool from "components/widgets/truenas/pool";
+import Pool from "widgets/truenas/pool";
 
 export default function Component({ service }) {
   const { t } = useTranslation();
