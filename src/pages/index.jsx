bf2870656ed7dcad871ba8a5c9e87841fd6684b3
shamoon <4887959+shamoon@users.noreply.github.com>
2024-02-28 01:30:20 -0800

    Fix: support cyrillic characters in quicklaunch

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -225,7 +225,7 @@ function Home({ initialSettings }) {
       if (e.target.tagName === "BODY" || e.target.id === "inner_wrapper") {
         if (
           (e.key.length === 1 &&
-            e.key.match(/(\w|\s|[à-ü]|[À-Ü])/g) &&
+            e.key.match(/(\w|\s|[à-ü]|[À-Ü]|[\w\u0430-\u044f])/gi) &&
             !(e.altKey || e.ctrlKey || e.metaKey || e.shiftKey)) ||
           e.key.match(/([à-ü]|[À-Ü])/g) || // accented characters may require modifier keys
           (e.key === "v" && (e.ctrlKey || e.metaKey))
