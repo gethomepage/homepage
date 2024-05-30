e4680f9b9c5322d5bad465966b15ebd16b8f0d8e
Devedse <davydavidse@gmail.com>
2024-05-30 15:18:32 +0200

    Added component

---
+++ src/widgets/develancacheui/widget.js
@@ -0,0 +1,14 @@
+import genericProxyHandler from "utils/proxy/handlers/generic";
+
+const widget = {
+  api: "{url}/{endpoint}",
+  proxyHandler: genericProxyHandler,
+
+  mappings: {
+    "DownloadStats/GetTotalDownloadStats": {
+      endpoint: "DownloadStats/GetTotalDownloadStats",
+    }
+  },
+};
+
+export default widget;
