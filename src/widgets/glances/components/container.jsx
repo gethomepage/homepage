b4a90fbf245804db334870cbdff70f7c2a5da255
shamoon <4887959+shamoon@users.noreply.github.com>
2024-06-22 13:06:11 -0700

    Update container.jsx

--- src/widgets/glances/components/container.jsx
+++ src/widgets/glances/components/container.jsx
@@ -8,13 +8,20 @@ export default function Container({ children, widget, error = null, chart = true
   const { settings } = useContext(SettingsContext);
   const hideErrors = settings.hideErrors || widget?.hideErrors;
 
+  if (error) {
+    if (hideErrors) {
+      return null;
+    }
+
+    return <Error />;
+  }
+
   return (
     <div>
       {children}
       <div className={`absolute top-0 right-0 bottom-0 left-0 overflow-clip pointer-events-none ${className}`} />
       {chart && <div className="h-[68px] overflow-clip" />}
       {!chart && <div className="h-[16px] overflow-clip" />}
-      {error && !hideErrors && <Error />}
     </div>
   );
 }
