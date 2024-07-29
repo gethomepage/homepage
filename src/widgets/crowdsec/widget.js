d67aa12012d93fadccf7a5628b2c87f6990cd0e5
Cyb3rzombie <me.github@cybernest.de>
2024-07-29 10:32:25 +0200

    Fix: query crowdsec api without limit

--- src/widgets/crowdsec/widget.js
+++ src/widgets/crowdsec/widget.js
@@ -7,10 +7,10 @@ const widget = {
 
   mappings: {
     alerts: {
-      endpoint: "alerts",
+      endpoint: "alerts?limit=0",
     },
     bans: {
-      endpoint: "alerts?decision_type=ban&origin=crowdsec&has_active_decision=1",
+      endpoint: "alerts?decision_type=ban&origin=crowdsec&has_active_decision=1&limit=0",
     },
   },
 };
