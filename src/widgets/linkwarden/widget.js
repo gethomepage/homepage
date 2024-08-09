2645e78fa7ab769829d4b3d300c915577ab466bb
shamoon <4887959+shamoon@users.noreply.github.com>
2024-08-09 14:06:20 -0700

    Remove unnecessary proxy

--- src/widgets/linkwarden/widget.js
+++ src/widgets/linkwarden/widget.js
@@ -1,8 +1,8 @@
-import linkwardenProxyHandler from "./proxy";
+import credentialedProxyHandler from "utils/proxy/handlers/credentialed";
 
 const widget = {
   api: "{url}/api/v1/{endpoint}",
-  proxyHandler: linkwardenProxyHandler,
+  proxyHandler: credentialedProxyHandler,
 
   mappings: {
     collections: {
