4d38222ba0d01d428588776974bba3054584eaf1
PyrokineticDarkElf <93349494+PyrokineticDarkElf@users.noreply.github.com>
2024-08-09 22:19:06 +0100

    Feature: Linkwarden service widget (#3836)
    Co-authored-by: shamoon <4887959+shamoon@users.noreply.github.com>

--- src/widgets/components.js
+++ src/widgets/components.js
@@ -54,6 +54,7 @@ const components = {
   komga: dynamic(() => import("./komga/component")),
   kopia: dynamic(() => import("./kopia/component")),
   lidarr: dynamic(() => import("./lidarr/component")),
+  linkwarden: dynamic(() => import("./linkwarden/component")),
   mastodon: dynamic(() => import("./mastodon/component")),
   mealie: dynamic(() => import("./mealie/component")),
   medusa: dynamic(() => import("./medusa/component")),
