b14374f660a2f804ab9f9377e3f027b8bca3c5f4
joncrangle <94425204+joncrangle@users.noreply.github.com>
2024-08-25 00:02:47 -0400

    Chore: handle mealie API change (#3895)
    ---------
    
    Co-authored-by: shamoon <4887959+shamoon@users.noreply.github.com>

--- src/widgets/mealie/widget.js
+++ src/widgets/mealie/widget.js
@@ -1,8 +1,17 @@
 import credentialedProxyHandler from "utils/proxy/handlers/credentialed";
 
 const widget = {
-  api: "{url}/api/groups/statistics",
+  api: "{url}/api/{endpoint}",
   proxyHandler: credentialedProxyHandler,
+
+  mappings: {
+    statisticsv1: {
+      endpoint: "groups/statistics",
+    },
+    statisticsv2: {
+      endpoint: "households/statistics",
+    },
+  },
 };
 
 export default widget;
