6324934faa7cf6302e5ef9d1f48f0cf542f23c7c
shamoon <4887959+shamoon@users.noreply.github.com>
2024-09-10 07:09:51 -0700

    Fix: other jsonrpc widgets broken in 0.9.8

--- src/utils/proxy/handlers/jsonrpc.js
+++ src/utils/proxy/handlers/jsonrpc.js
@@ -14,11 +14,11 @@ export async function sendJsonRpcRequest(url, method, params, widget) {
     accept: "application/json",
   };
 
-  if (widget.username && widget.password) {
+  if (widget?.username && widget?.password) {
     headers.Authorization = `Basic ${Buffer.from(`${widget.username}:${widget.password}`).toString("base64")}`;
   }
 
-  if (widget.key) {
+  if (widget?.key) {
     headers.Authorization = `Bearer ${widget.key}`;
   }
 
