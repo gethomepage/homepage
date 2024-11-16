601ba4cd8cf102a7da783d56f405457a5d44bace
Urs Kröll <dev@urskroell.de>
2024-11-16 03:11:10 +0100

    set correct gitlab api version

--- src/widgets/gitlab/widget.js
+++ src/widgets/gitlab/widget.js
@@ -2,7 +2,7 @@ import { asJson } from "utils/proxy/api-helpers";
 import credentialedProxyHandler from "utils/proxy/handlers/credentialed";
 
 const widget = {
-  api: "{url}/api/v1/{endpoint}",
+  api: "{url}/api/v4/{endpoint}",
   proxyHandler: credentialedProxyHandler,
   mappings: {
     events: {
