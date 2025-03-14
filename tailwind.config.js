dc28c7a06096846a1ab84e95bac3a4f3ae1407ce
shamoon <4887959+shamoon@users.noreply.github.com>
2025-03-14 12:19:38 -0700

    Fix: fix fiveColumns option

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,10 +30,6 @@ module.exports = {
           900: "rgb(var(--color-900) / <alpha-value>)",
         },
       },
-      screens: {
-        "3xl": "1800px",
-        // => @media (min-width: 1800px) { ... }
-      },
     },
   },
   plugins: [tailwindForms, tailwindScrollbars],
