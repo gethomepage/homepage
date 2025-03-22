3437326a1f0b0294581bb8b75cf6280276999471
chiragkrishna <chiragkrishna@gmail.com>
2025-03-22 16:14:37 +0530

    Update component.jsx
    Add Repositories field to Gitea

--- src/widgets/gitea/component.jsx
+++ src/widgets/gitea/component.jsx
@@ -1,6 +1,5 @@
 import Container from "components/services/widget/container";
 import Block from "components/services/widget/block";
-
 import useWidgetAPI from "utils/proxy/use-widget-api";
 
 export default function Component({ service }) {
@@ -8,17 +7,19 @@ export default function Component({ service }) {
 
   const { data: giteaNotifications, error: giteaNotificationsError } = useWidgetAPI(widget, "notifications");
   const { data: giteaIssues, error: giteaIssuesError } = useWidgetAPI(widget, "issues");
+  const { data: giteaRepositories, error: giteaRepositoriesError } = useWidgetAPI(widget, "repositories");
 
-  if (giteaNotificationsError || giteaIssuesError) {
-    return <Container service={service} error={giteaNotificationsError ?? giteaIssuesError} />;
+  if (giteaNotificationsError || giteaIssuesError || giteaRepositoriesError) {
+    return <Container service={service} error={giteaNotificationsError ?? giteaIssuesError ?? giteaRepositoriesError} />;
   }
 
-  if (!giteaNotifications || !giteaIssues) {
+  if (!giteaNotifications || !giteaIssues || !giteaRepositories) {
     return (
       <Container service={service}>
         <Block label="gitea.notifications" />
         <Block label="gitea.issues" />
         <Block label="gitea.pulls" />
+        <Block label="gitea.repositories" />
       </Container>
     );
   }
@@ -28,6 +29,7 @@ export default function Component({ service }) {
       <Block label="gitea.notifications" value={giteaNotifications.length} />
       <Block label="gitea.issues" value={giteaIssues.issues.length} />
       <Block label="gitea.pulls" value={giteaIssues.pulls.length} />
+      <Block label="gitea.repositories" value={giteaRepositories} />
     </Container>
   );
 }
