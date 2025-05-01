165d2de0d312029afc37eae645cee9331d19640a
InsertDisc <31751462+InsertDisc@users.noreply.github.com>
2025-05-01 19:24:13 -0400

    Update component.jsx

--- src/widgets/satisfactory/component.jsx
+++ src/widgets/satisfactory/component.jsx
@@ -1,9 +1,10 @@
 import { useTranslation } from "next-i18next";
 
-import { formatDuration, formatInternalName } from "./transforms";
-
 import Block from "components/services/widget/block";
 import Container from "components/services/widget/container";
+
+import { formatDuration, formatInternalName } from "./transforms";
+
 import useWidgetAPI from "utils/proxy/use-widget-api";
 
 export default function Component({ service }) {
