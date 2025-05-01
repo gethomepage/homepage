3b7b9d69b6c375bcf48f81f9d9e847b7dcd95f9b
InsertDisc <31751462+InsertDisc@users.noreply.github.com>
2025-05-01 18:59:37 -0400

    Update proxy.js
    Refactor for Lint compliance.

--- src/widgets/satisfactory/proxy.js
+++ src/widgets/satisfactory/proxy.js
@@ -1,8 +1,8 @@
+import getServiceWidget from "utils/config/service-helpers";
+import createLogger from "utils/logger";
 import { formatApiCall } from "utils/proxy/api-helpers";
 import { httpProxy } from "utils/proxy/http";
-import createLogger from "utils/logger";
 import widgets from "widgets/widgets";
-import getServiceWidget from "utils/config/service-helpers";
 
 const logger = createLogger("satisfactoryProxyHandler");
 
@@ -20,11 +20,11 @@ export default async function satisfactoryProxyHandler(req, res) {
     return res.status(403).json({ error: "Service does not support API calls" });
   }
 
-  const url = formatApiCall(api, {...widget});
+  const url = formatApiCall(api, { ...widget });
   const method = "POST";
   const payload = { function: "QueryServerState" };
   const headers = {
-    "Authorization": `Bearer ${widget.key}`,
+    Authorization: `Bearer ${widget.key}`,
     "Content-Type": "application/json",
   };
 
@@ -34,6 +34,7 @@ export default async function satisfactoryProxyHandler(req, res) {
       body: JSON.stringify(payload),
       headers,
     });
+
 
     const data = JSON.parse(responseData.toString('utf8')).data.serverGameState;
 
