1c982aefc2803f087f6641b4be8dd2cf4a945798
Stancu Florin <florin@stancu.me>
2025-05-18 14:07:04 +0300

    fix lint issues

--- src/components/services/group.jsx
+++ src/components/services/group.jsx
@@ -41,8 +41,7 @@ export default function ServicesGroup({
         layout?.style !== "row" ? getColumnBasisClasses(maxGroupColumns) : "",
         groupPadding,
         isSubgroup ? "subgroup" : "",
-        )
-      }
+      )}
     >
       <Disclosure defaultOpen={!(layout?.initiallyCollapsed ?? groupsInitiallyCollapsed) ?? true}>
         {({ open }) => (
