da42153c8aa0c4b6dfd00689be22d6cc32154e08
Dmitry Chumak <mega.venik@gmail.com>
2025-05-22 00:29:12 +0300

    linter fixes part 2

--- src/widgets/checkmk/widget.js
+++ src/widgets/checkmk/widget.js
@@ -1,15 +1,15 @@
 import credentialedProxyHandler from "utils/proxy/handlers/credentialed";
 
-const widget =  {
+const widget = {
   api: "{url}/{site}/check_mk/api/1.0/{endpoint}",
   proxyHandler: credentialedProxyHandler,
 
-  mappings:  {
-    services_info:  {
+  mappings: {
+    services_info: {
       endpoint: "domain-types/service/collections/all",
       params: ["columns", "query"],
     },
-    hosts_info:  {
+    hosts_info: {
       endpoint: "domain-types/host/collections/all",
       params: ["columns", "query"],
     },
