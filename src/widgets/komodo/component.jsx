8d0baf5ad645bad09cc02c5d9076cf0c17631259
Andrew Ensley <aensley@users.noreply.github.com>
2025-06-10 14:02:28 -0400

    fix linting error

--- src/widgets/komodo/component.jsx
+++ src/widgets/komodo/component.jsx
@@ -1,6 +1,8 @@
 import Block from "components/services/widget/block";
 import Container from "components/services/widget/container";
+
 import { useTranslation } from "next-i18next";
+
 import useWidgetAPI from "utils/proxy/use-widget-api";
 
 export default function Component({ service }) {
