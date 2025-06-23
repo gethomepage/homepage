f6312c981fd0eb1c787e0bc83e004c3b28b20c55
AdamWHY2K <github@adamwhy2k.anonaddy.com>
2025-06-23 08:05:04 +0100

    feat: add jellyseerr issue field

--- src/widgets/jellyseerr/component.jsx
+++ src/widgets/jellyseerr/component.jsx
@@ -7,17 +7,18 @@ export default function Component({ service }) {
   const { widget } = service;
 
   const { data: statsData, error: statsError } = useWidgetAPI(widget, "request/count");
-
-  if (statsError) {
-    return <Container service={service} error={statsError} />;
+  const { data: issueData, error: issueError } = useWidgetAPI(widget, "issue/count");
+  if (statsError || issueError) {
+    return <Container service={service} error={statsError ? statsError : issueError} />;
   }
 
-  if (!statsData) {
+  if (!statsData || !issueData) {
     return (
       <Container service={service}>
         <Block label="jellyseerr.pending" />
         <Block label="jellyseerr.approved" />
         <Block label="jellyseerr.available" />
+        <Block label="jellyseerr.issues" />
       </Container>
     );
   }
@@ -27,6 +28,7 @@ export default function Component({ service }) {
       <Block label="jellyseerr.pending" value={statsData.pending} />
       <Block label="jellyseerr.approved" value={statsData.approved} />
       <Block label="jellyseerr.available" value={statsData.available} />
+      <Block label="jellyseerr.issues" value={`${issueData.open} / ${issueData.total}`} />
     </Container>
   );
 }
