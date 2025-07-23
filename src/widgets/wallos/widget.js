173615943b3b79faceb4f2ee1612180eb183483e
MH <m@terramail.co>
2025-07-23 18:13:54 +0100

    Fixed the API endpoint to include the  on the  and not in the  after seeing the end-result of the behaviour of the proxy handler with adding query params to the base path when it already includes one, i.e.  - the  will end up in the middle which is understandable. Also fixed  to  as the [Wallos documentation was wrong](https://github.com/ellite/Wallos/issues/750).

--- src/widgets/wallos/widget.js
+++ src/widgets/wallos/widget.js
@@ -1,12 +1,12 @@
 import genericProxyHandler from "utils/proxy/handlers/generic";
 
 const widget = {
-  api: "{url}/api/{endpoint}.php?apiKey={key}",
+  api: "{url}/api/{endpoint}?api_key={key}",
   proxyHandler: genericProxyHandler,
 
   mappings: {
     "subscriptions/get_monthly_cost": {
-      endpoint: "subscriptions/get_monthly_cost",
+      endpoint: "subscriptions/get_monthly_cost.php",
       validate: [
         "localized_monthly_cost",
         "currency_symbol"
