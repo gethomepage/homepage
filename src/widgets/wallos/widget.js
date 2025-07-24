44fda7e92787af77cb56332ce599ea480f9dab3b
Matan Heimann <matan.heimann@gmail.com>
2025-07-24 06:40:27 +0100

    Update src/widgets/wallos/widget.js
    Co-authored-by: shamoon <4887959+shamoon@users.noreply.github.com>

--- src/widgets/wallos/widget.js
+++ src/widgets/wallos/widget.js
@@ -5,7 +5,7 @@ const widget = {
   proxyHandler: genericProxyHandler,
 
   mappings: {
-    "subscriptions/get_monthly_cost": {
+    "get_monthly_cost": {
       endpoint: "subscriptions/get_monthly_cost.php",
       validate: ["localized_monthly_cost", "currency_symbol"],
       params: ["month", "year"],
