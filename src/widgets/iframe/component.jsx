bb23c25690556359390890c4f7617d41e1218577
shamoon <4887959+shamoon@users.noreply.github.com>
2025-08-04 23:01:26 -0400

    Fix: try to restore iframe transparency by restoring light scheme (#5637)

--- src/widgets/iframe/component.jsx
+++ src/widgets/iframe/component.jsx
@@ -24,7 +24,7 @@ export default function Component({ service }) {
     <Container service={service}>
       <div
         className={classNames(
-          "bg-theme-200/50 dark:bg-theme-900/20 rounded-sm m-1 flex-1 flex flex-col items-center justify-center text-center",
+          "bg-theme-200/50 dark:bg-theme-900/20 rounded-sm m-1 flex-1 flex flex-col items-center justify-center text-center scheme-light",
           "service-block",
         )}
       >
