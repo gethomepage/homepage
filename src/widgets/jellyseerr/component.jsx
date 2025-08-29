a21694d2d236d868e139aad541689fd1b0fd67c1
AdamWHY2K <adamgm.email@gmail.com>
2025-08-29 17:16:04 +0100

    refactor: conditional rendering & issue field required for api request

--- src/widgets/jellyseerr/component.jsx
+++ src/widgets/jellyseerr/component.jsx
@@ -5,30 +5,33 @@ import useWidgetAPI from "utils/proxy/use-widget-api";
 
 export default function Component({ service }) {
   const { widget } = service;
+  const isIssueEnabled = widget.fields.includes("issues");
 
   const { data: statsData, error: statsError } = useWidgetAPI(widget, "request/count");
-  const { data: issueData, error: issueError } = useWidgetAPI(widget, "issue/count");
-  if (statsError || issueError) {
+  const { data: issueData, error: issueError } = useWidgetAPI(widget, isIssueEnabled ? "issue/count" : "");
+  if (statsError || (isIssueEnabled && !issueData)) {
     return <Container service={service} error={statsError ? statsError : issueError} />;
   }
 
-  if (!statsData || !issueData) {
+  if (!statsData || (isIssueEnabled && !issueData)) {
     return (
       <Container service={service}>
-        <Block label="jellyseerr.pending" />
-        <Block label="jellyseerr.approved" />
-        <Block label="jellyseerr.available" />
-        <Block label="jellyseerr.issues" />
+        {widget.fields.includes("pending") && <Block label="jellyseerr.pending" />}
+        {widget.fields.includes("approved") && <Block label="jellyseerr.approved" />}
+        {widget.fields.includes("available") && <Block label="jellyseerr.available" />}
+        {widget.fields.includes("issues") && <Block label="jellyseerr.issues" />}
       </Container>
     );
   }
 
   return (
     <Container service={service}>
-      <Block label="jellyseerr.pending" value={statsData.pending} />
-      <Block label="jellyseerr.approved" value={statsData.approved} />
-      <Block label="jellyseerr.available" value={statsData.available} />
-      <Block label="jellyseerr.issues" value={`${issueData.open} / ${issueData.total}`} />
+      {widget.fields.includes("pending") && <Block label="jellyseerr.pending" value={statsData.pending} />}
+      {widget.fields.includes("approved") && <Block label="jellyseerr.approved" value={statsData.approved} />}
+      {widget.fields.includes("available") && <Block label="jellyseerr.available" value={statsData.available} />}
+      {widget.fields.includes("issues") && (
+        <Block label="jellyseerr.issues" value={`${issueData.open} / ${issueData.total}`} />
+      )}
     </Container>
   );
 }
