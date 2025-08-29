b663e561748da2664d15bc396c52679966460e8c
AdamWHY2K <adamgm.email@gmail.com>
2025-08-29 19:14:13 +0100

    Enhancement: Add issues field to Jellyseerr widget (#5725)
    Co-authored-by: shamoon <4887959+shamoon@users.noreply.github.com>

--- src/widgets/jellyseerr/component.jsx
+++ src/widgets/jellyseerr/component.jsx
@@ -3,21 +3,27 @@ import Container from "components/services/widget/container";
 
 import useWidgetAPI from "utils/proxy/use-widget-api";
 
+export const jellyseerrDefaultFields = ["pending", "approved", "available"];
+
 export default function Component({ service }) {
   const { widget } = service;
 
-  const { data: statsData, error: statsError } = useWidgetAPI(widget, "request/count");
+  widget.fields = widget?.fields?.length ? widget.fields : jellyseerrDefaultFields;
+  const isIssueEnabled = widget.fields.includes("issues");
 
-  if (statsError) {
-    return <Container service={service} error={statsError} />;
+  const { data: statsData, error: statsError } = useWidgetAPI(widget, "request/count");
+  const { data: issueData, error: issueError } = useWidgetAPI(widget, isIssueEnabled ? "issue/count" : "");
+  if (statsError || (isIssueEnabled && issueError)) {
+    return <Container service={service} error={statsError ? statsError : issueError} />;
   }
 
-  if (!statsData) {
+  if (!statsData || (isIssueEnabled && !issueData)) {
     return (
       <Container service={service}>
         <Block label="jellyseerr.pending" />
         <Block label="jellyseerr.approved" />
         <Block label="jellyseerr.available" />
+        <Block label="jellyseerr.issues" />
       </Container>
     );
   }
@@ -27,6 +33,7 @@ export default function Component({ service }) {
       <Block label="jellyseerr.pending" value={statsData.pending} />
       <Block label="jellyseerr.approved" value={statsData.approved} />
       <Block label="jellyseerr.available" value={statsData.available} />
+      <Block label="jellyseerr.issues" value={`${issueData?.open} / ${issueData?.total}`} />
     </Container>
   );
 }
