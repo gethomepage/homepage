aa7cab0351afe3ddc24885695bd820a576b94c4b
Benjamin Rumble <therumbler@gmail.com>
2025-10-23 13:51:34 -0400

    Allow both LAN and WLAN blocks to display
    Before this change some LAN stats would not show if WLAN was showing, and some WLAN stats would not show if LAN stats were showing.
    
    This change allows both WLAN and LAN stats to display, regardless of the status of the other

--- src/widgets/unifi/component.jsx
+++ src/widgets/unifi/component.jsx
@@ -62,16 +62,16 @@ export default function Component({ service }) {
       {wan.show && <Block label="unifi.wan" value={wan.status === "ok" ? t("unifi.up") : t("unifi.down")} />}
 
       {lan.show && <Block label="unifi.lan_users" value={t("common.number", { value: lan.num_user })} />}
-      {lan.show && !wlan.show && (
+      {lan.show && (
         <Block label="unifi.lan_devices" value={t("common.number", { value: lan.num_adopted })} />
       )}
-      {lan.show && !wlan.show && <Block label="unifi.lan" value={lan.up ? t("unifi.up") : t("unifi.down")} />}
+      {lan.show && <Block label="unifi.lan" value={lan.up ? t("unifi.up") : t("unifi.down")} />}
 
       {wlan.show && <Block label="unifi.wlan_users" value={t("common.number", { value: wlan.num_user })} />}
-      {wlan.show && !lan.show && (
+      {wlan.show && (
         <Block label="unifi.wlan_devices" value={t("common.number", { value: wlan.num_adopted })} />
       )}
-      {wlan.show && !lan.show && <Block label="unifi.wlan" value={wlan.up ? t("unifi.up") : t("unifi.down")} />}
+      {wlan.show && <Block label="unifi.wlan" value={wlan.up ? t("unifi.up") : t("unifi.down")} />}
     </Container>
   );
 }
