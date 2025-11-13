d446389398b32a79b6da6ddaa575a2034cabe15f
shamoon <4887959+shamoon@users.noreply.github.com>
2025-11-12 16:21:36 -0800

    DRY

--- src/widgets/ghostfolio/widget.js
+++ src/widgets/ghostfolio/widget.js
@@ -1,21 +1,21 @@
 import credentialedProxyHandler from "utils/proxy/handlers/credentialed";
 
 const widget = {
-  api: "{url}/{endpoint}",
+  api: "{url}/api/{endpoint}",
   proxyHandler: credentialedProxyHandler,
 
   mappings: {
     today: {
-      endpoint: "api/v2/portfolio/performance?range=1d",
+      endpoint: "v2/portfolio/performance?range=1d",
     },
     year: {
-      endpoint: "api/v2/portfolio/performance?range=1y",
+      endpoint: "v2/portfolio/performance?range=1y",
     },
     max: {
-      endpoint: "api/v2/portfolio/performance?range=max",
+      endpoint: "v2/portfolio/performance?range=max",
     },
     userInfo: {
-      endpoint: "api/v1/user",
+      endpoint: "v1/user",
     },
   },
 };
