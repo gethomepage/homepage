4b1cce726954f0614af0ee5aa93e4b7062c3ad7b
shamoon <4887959+shamoon@users.noreply.github.com>
2026-01-04 00:18:46 -0800

    [revert] more logging

--- src/widgets/truenas/proxy.js
+++ src/widgets/truenas/proxy.js
@@ -30,6 +30,7 @@ function waitForEvent(ws, handler, { event = "message", parseJson = true } = {})
       try {
         const parsed = parseJson ? JSON.parse(payload.toString()) : payload;
         if (parseJson) logger.info("Received TrueNAS websocket message: %o", parsed);
+        else logger.info("Received TrueNAS websocket message: %o", payload);
         const handlerResult = handler(parsed);
         if (handlerResult !== undefined) {
           cleanup();
