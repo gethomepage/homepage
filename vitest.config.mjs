67307fd02f388b50deada0de923e3aad984514fe
shamoon <4887959+shamoon@users.noreply.github.com>
2026-02-02 21:23:53 -0800

    Resolve some paths

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,6 +1,14 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      components: fileURLToPath(new URL("./src/components", import.meta.url)),
+      utils: fileURLToPath(new URL("./src/utils", import.meta.url)),
+      widgets: fileURLToPath(new URL("./src/widgets", import.meta.url)),
+    },
+  },
   test: {
     environment: "node",
     setupFiles: ["./vitest.setup.js"],
