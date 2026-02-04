ed54a2284151bf594753788191804a50fc1f892c
shamoon <4887959+shamoon@users.noreply.github.com>
2026-02-04 10:06:19 -0800

    test: add widget config tests (batch 6)

--- src/widgets/plantit/widget.js
+++ src/widgets/plantit/widget.js
@@ -1,4 +1,3 @@
-import { asJson } from "utils/proxy/api-helpers";
 import credentialedProxyHandler from "utils/proxy/handlers/credentialed";
 
 const widget = {
@@ -9,12 +8,6 @@ const widget = {
     plantit: {
       endpoint: "stats",
     },
-    map: (data) => ({
-      events: Object.values(asJson(data).diaryEntryCount).reduce((acc, i) => acc + i, 0),
-      plants: Object.values(asJson(data).plantCount).reduce((acc, i) => acc + i, 0),
-      photos: Object.values(asJson(data).imageCount).reduce((acc, i) => acc + i, 0),
-      species: Object.values(asJson(data).botanicalInfoCount).reduce((acc, i) => acc + i, 0),
-    }),
   },
 };
 
