24cb274e033bed67bf03d42e17fb557d293c486f
shamoon <4887959+shamoon@users.noreply.github.com>
2026-04-01 08:02:03 -0700

    Fix glances regex

--- src/widgets/glances/widget.test.js
+++ src/widgets/glances/widget.test.js
@@ -8,6 +8,10 @@ describe("glances widget config", () => {
   it("exports a valid widget config", () => {
     expectWidgetConfigShape(widget);
     expect(widget.allowedEndpoints?.test("3/quicklook")).toBe(true);
+    expect(widget.allowedEndpoints?.test("12/cpu")).toBe(true);
     expect(widget.allowedEndpoints?.test("unknown")).toBe(false);
+    expect(widget.allowedEndpoints?.test("xxcpuyy")).toBe(false);
+    expect(widget.allowedEndpoints?.test("3/cpu/extra")).toBe(false);
+    expect(widget.allowedEndpoints?.test("membrane")).toBe(false);
   });
 });
