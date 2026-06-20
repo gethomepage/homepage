3f5577eca3747917646082b8badc5329c2d4d307
John Reiner <johnreiner307@gmail.com>
2026-06-20 16:16:59 -0400

    Enhancement: support qBittorrent apikey

--- src/widgets/qbittorrent/proxy.js
+++ src/widgets/qbittorrent/proxy.js
@@ -9,11 +9,18 @@ async function login(widget) {
   logger.debug("qBittorrent is rejecting the request, logging in.");
   const loginUrl = new URL(`${widget.url}/api/v2/auth/login`).toString();
   const loginBody = `username=${encodeURIComponent(widget.username)}&password=${encodeURIComponent(widget.password)}`;
+  const loginKey = `${encodeURIComponent(widget.key)}`;
   const loginParams = {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: loginBody,
+    body: null,
   };
+
+  if (widget.key) {
+    loginParams.headers.Authorization = `Bearer ${loginKey}`;
+  } else if (widget.username && widget.password) {
+    loginParams.body = loginBody
+  }
 
   const [status, contentType, data] = await httpProxy(loginUrl, loginParams);
   return [status, data];
