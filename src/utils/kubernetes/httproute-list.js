58dd68bd62be770b16f870a0bdc614b7d826ff8f
Elie Mouawad <elie.mouawad@telus.com>
2026-08-08 00:35:06 +0300

    perf(api): reduce calls to kubernetes api-server

--- src/utils/kubernetes/httproute-list.js
+++ src/utils/kubernetes/httproute-list.js
@@ -7,49 +7,29 @@ const logger = createLogger("httproute-list");
 const kc = getKubeConfig();
 
 export default async function listHttpRoute() {
-  const crd = kc.makeApiClient(CustomObjectsApi);
-  const core = kc.makeApiClient(CoreV1Api);
   const { gateway } = getKubernetes();
-  let httpRouteList = [];
 
   if (gateway) {
-    // httproutes
-    const getHttpRoutes = async (namespace) =>
-      crd
-        .listNamespacedCustomObject({
+    const crd = kc.makeApiClient(CustomObjectsApi);
+
+    const httpRoutes = await crd
+        .listClusterCustomObject({
           group: HTTPROUTE_API_GROUP,
           version: HTTPROUTE_API_VERSION,
-          namespace,
           plural: "httproutes",
         })
         .then((response) => {
-          return response.items;
+          logger.debug("Retrieved httproutes: %d", response?.items?.length ?? 0);
+          return response?.items;
         })
         .catch((error) => {
           logger.error("Error getting httproutes: %d %s %s", error.statusCode, error.body, error.response);
           logger.debug(error);
           return null;
         });
-    // namespaces
-    const namespaces = await core
-      .listNamespace()
-      .then((response) => response.items.map((ns) => ns.metadata.name))
-      .catch((error) => {
-        logger.error("Error getting namespaces: %d %s %s", error.statusCode, error.body, error.response);
-        logger.debug(error);
-        return null;
-      });
 
-    if (namespaces) {
-      const httpRouteListUnfiltered = await Promise.all(
-        namespaces.map(async (namespace) => {
-          const httpRoutes = await getHttpRoutes(namespace);
-          return httpRoutes;
-        }),
-      );
-
-      httpRouteList = httpRouteListUnfiltered.flat().filter((httpRoute) => httpRoute);
-    }
+    return httpRoutes;
   }
-  return httpRouteList;
+
+  return [];
 }
