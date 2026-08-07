c99bd4947d5e8d54c2ca997e3037b9a12e449677
Elie Mouawad <elie.mouawad@telus.com>
2026-08-08 01:36:01 +0300

    fix linting - prettier

--- src/utils/kubernetes/httproute-list.js
+++ src/utils/kubernetes/httproute-list.js
@@ -1,4 +1,4 @@
-import { CoreV1Api, CustomObjectsApi } from "@kubernetes/client-node";
+import { CustomObjectsApi } from "@kubernetes/client-node";
 
 import { getKubeConfig, getKubernetes, HTTPROUTE_API_GROUP, HTTPROUTE_API_VERSION } from "utils/config/kubernetes";
 import createLogger from "utils/logger";
@@ -13,20 +13,20 @@ export default async function listHttpRoute() {
     const crd = kc.makeApiClient(CustomObjectsApi);
 
     const httpRoutes = await crd
-        .listClusterCustomObject({
-          group: HTTPROUTE_API_GROUP,
-          version: HTTPROUTE_API_VERSION,
-          plural: "httproutes",
-        })
-        .then((response) => {
-          logger.debug("Retrieved httproutes: %d", response?.items?.length ?? 0);
-          return response?.items;
-        })
-        .catch((error) => {
-          logger.error("Error getting httproutes: %d %s %s", error.statusCode, error.body, error.response);
-          logger.debug(error);
-          return [];
-        });
+      .listClusterCustomObject({
+        group: HTTPROUTE_API_GROUP,
+        version: HTTPROUTE_API_VERSION,
+        plural: "httproutes",
+      })
+      .then((response) => {
+        logger.debug("Retrieved httproutes: %d", response?.items?.length ?? 0);
+        return response?.items;
+      })
+      .catch((error) => {
+        logger.error("Error getting httproutes: %d %s %s", error.statusCode, error.body, error.response);
+        logger.debug(error);
+        return [];
+      });
 
     return httpRoutes;
   }
