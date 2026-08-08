1a047d1dfc135fd3c904699f562dd9ea892088e6
Elie Mouawad <6771272+emouawad@users.noreply.github.com>
2026-08-08 04:43:11 +0300

    Performance: reduce calls to kubernetes api-server (#6963)
    Co-authored-by: shamoon <4887959+shamoon@users.noreply.github.com>

--- src/utils/kubernetes/httproute-list.js
+++ src/utils/kubernetes/httproute-list.js
@@ -1,4 +1,4 @@
-import { CoreV1Api, CustomObjectsApi } from "@kubernetes/client-node";
+import { CustomObjectsApi } from "@kubernetes/client-node";
 
 import { getKubeConfig, getKubernetes, HTTPROUTE_API_GROUP, HTTPROUTE_API_VERSION } from "utils/config/kubernetes";
 import createLogger from "utils/logger";
@@ -7,49 +7,28 @@ const logger = createLogger("httproute-list");
 const kc = getKubeConfig();
 
 export default async function listHttpRoute() {
-  const crd = kc.makeApiClient(CustomObjectsApi);
-  const core = kc.makeApiClient(CoreV1Api);
   const { gateway } = getKubernetes();
-  let httpRouteList = [];
 
   if (gateway) {
-    // httproutes
-    const getHttpRoutes = async (namespace) =>
-      crd
-        .listNamespacedCustomObject({
-          group: HTTPROUTE_API_GROUP,
-          version: HTTPROUTE_API_VERSION,
-          namespace,
-          plural: "httproutes",
-        })
-        .then((response) => {
-          return response.items;
-        })
-        .catch((error) => {
-          logger.error("Error getting httproutes: %d %s %s", error.statusCode, error.body, error.response);
-          logger.debug(error);
-          return null;
-        });
-    // namespaces
-    const namespaces = await core
-      .listNamespace()
-      .then((response) => response.items.map((ns) => ns.metadata.name))
+    const crd = kc.makeApiClient(CustomObjectsApi);
+
+    const httpRoutes = await crd
+      .listClusterCustomObject({
+        group: HTTPROUTE_API_GROUP,
+        version: HTTPROUTE_API_VERSION,
+        plural: "httproutes",
+      })
+      .then((response) => {
+        return response?.items ?? [];
+      })
       .catch((error) => {
-        logger.error("Error getting namespaces: %d %s %s", error.statusCode, error.body, error.response);
+        logger.error("Error getting httproutes: %d %s %s", error.statusCode, error.body, error.response);
         logger.debug(error);
-        return null;
+        return [];
       });
 
-    if (namespaces) {
-      const httpRouteListUnfiltered = await Promise.all(
-        namespaces.map(async (namespace) => {
-          const httpRoutes = await getHttpRoutes(namespace);
-          return httpRoutes;
-        }),
-      );
-
-      httpRouteList = httpRouteListUnfiltered.flat().filter((httpRoute) => httpRoute);
-    }
+    return httpRoutes;
   }
-  return httpRouteList;
+
+  return [];
 }
